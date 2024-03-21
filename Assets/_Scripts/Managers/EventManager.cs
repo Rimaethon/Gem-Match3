3cc1bb73c0f5e5e1b73886efcd4ce02a1a44d549
Yoldaş Ulaş <yutalas@gmail.com>
2024-03-21 19:57:38 +0300

    First version of Gameplay Scene and A simple UI added

---
+++ Assets/_Scripts/Managers/EventManager.cs
@@ -0,0 +1,148 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Rimaethon.Scripts.Utility;
+using UnityEngine;
+
+namespace Rimaethon.Scripts.Managers
+{
+    public class EventManager : PersistentSingleton<EventManager>
+    {
+        #region Fields And Properties
+        private readonly Dictionary<GameEvents, List<Delegate>> _eventHandlers = new();
+        [SerializeField] private bool showEventNames; 
+        #endregion
+
+
+        #region Unity Methods
+
+        [SerializeField] private List<string> eventNames = new();
+
+        private void Start()
+        {
+            if (!showEventNames) return;
+            foreach (var events in _eventHandlers.Keys)
+            {
+                eventNames.Add(events.ToString());
+                foreach (var value in _eventHandlers[events].ToList())
+                {
+                    Type type = value.Method.DeclaringType;
+                    if (type == null) continue;
+                    string className = type.Name;
+                    eventNames.Add(className+ "." + value.Method.Name);
+                }
+               
+            }
+
+        }
+
+        protected override void OnApplicationQuit()
+        {
+            _eventHandlers.Clear();
+            Debug.LogWarning("Event Manager is cleared");
+            base.OnApplicationQuit();
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        public void AddHandler(GameEvents gameEvent, Action handler)
+        {
+            if (!_eventHandlers.ContainsKey(gameEvent)) _eventHandlers[gameEvent] = new List<Delegate>();
+
+            _eventHandlers[gameEvent].Add(handler);
+            Debug.Log($"Added handler {handler.Method.Name} for game event {gameEvent}");
+        }
+
+        public void AddHandler<T>(GameEvents gameEvent, Action<T> handler)
+        {
+            if (!_eventHandlers.ContainsKey(gameEvent)) _eventHandlers[gameEvent] = new List<Delegate>();
+
+            _eventHandlers[gameEvent].Add(handler);
+            Debug.Log($"Added handler {handler.Method.Name} for game event {gameEvent}");
+        }
+        public void AddHandler<T,T1>(GameEvents gameEvent, Action<T,T1> handler)
+        {
+            if (!_eventHandlers.ContainsKey(gameEvent)) _eventHandlers[gameEvent] = new List<Delegate>();
+
+            _eventHandlers[gameEvent].Add(handler);
+            Debug.Log($"Added handler {handler.Method.Name} for game event {gameEvent}");
+        }
+
+        public void RemoveHandler(GameEvents gameEvent, Action handler)
+        {
+            if (_eventHandlers.TryGetValue(gameEvent, out var handlers))
+            {
+                handlers.Remove(handler);
+                Debug.Log($"Removed handler {handler.Method.Name} for game event {gameEvent}");
+
+                if (handlers.Count == 0)
+                {
+                    _eventHandlers.Remove(gameEvent);
+                    Debug.Log($"No more handlers for game event {gameEvent}");
+                }
+            }
+        }
+
+        public void RemoveHandler<T>(GameEvents gameEvent, Action<T> handler)
+        {
+            if (_eventHandlers.TryGetValue(gameEvent, out var handlers))
+            {
+                handlers.Remove(handler);
+                Debug.Log($"Removed handler {handler.Method.Name} for game event {gameEvent}");
+
+                if (handlers.Count == 0)
+                {
+                    _eventHandlers.Remove(gameEvent);
+                    Debug.Log($"No more handlers for game event {gameEvent}");
+                }
+            }
+        }
+        public void RemoveHandler<T,T1>(GameEvents gameEvent, Action<T,T1> handler)
+        {
+            if (_eventHandlers.TryGetValue(gameEvent, out var handlers))
+            {
+                handlers.Remove(handler);
+                Debug.Log($"Removed handler {handler.Method.Name} for game event {gameEvent}");
+
+                if (handlers.Count == 0)
+                {
+                    _eventHandlers.Remove(gameEvent);
+                    Debug.Log($"No more handlers for game event {gameEvent}");
+                }
+            }
+        }
+        #endregion
+
+        #region Event Broadcasting
+
+        public void Broadcast(GameEvents gameEvents)
+        {
+            ProcessEvent(gameEvents);
+        }
+
+        public void Broadcast<T>(GameEvents gameEvent, T arg)
+        {
+            ProcessEvent(gameEvent, arg);
+        }
+
+        public void Broadcast<T,T1>(GameEvents gameEvent, T arg, T1 arg1)
+        {
+            ProcessEvent(gameEvent, arg, arg1);
+        }
+    
+        private void ProcessEvent(GameEvents gameEvents, params object[] args)
+        {
+            if (_eventHandlers.TryGetValue(gameEvents, out var eventHandler))
+                foreach (var handler in eventHandler)
+                {
+                    handler.DynamicInvoke(args);
+                    Debug.Log(
+                        $"Broadcasted event {gameEvents} with arguments {string.Join(", ", args.Select(arg => arg.ToString()))} to handler {handler.Method.Name}");
+                }
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/_Scripts/Managers/ObjectPool.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    
+    private Dictionary<int, Stack<GameObject>> items;
+    private Dictionary<int, Stack<GameObject>> particleEffects;
+    
+    [SerializeField] private ItemDatabaseSO itemDatabase;
+
+    private void Awake()
+    {
+        items = new Dictionary<int, Stack<GameObject>>();
+        particleEffects= new Dictionary<int, Stack<GameObject>>();
+        for (int i = 0; i < itemDatabase.BoardElements.Count; i++)
+        {
+            items[i] = new Stack<GameObject>();
+        }
+        for (int i = 0; i < itemDatabase.ParticleEffects.Count; i++)
+        {
+            particleEffects[i] = new Stack<GameObject>();
+        }
+
+    }
+
+    public GameObject GetItemFromPool(int itemID, Vector3 position)
+    {
+      
+        if (items[itemID].Count > 0)
+        {
+            return items[itemID].Pop();
+        }
+        else
+        {
+            GameObject prefab = Instantiate(itemDatabase.GetBoardElement(itemID), position, Quaternion.identity);
+            return prefab;
+        }
+    }
+    
+    public GameObject GetParticleEffectFromPool(int itemID, Vector3 position)
+    {
+        if (particleEffects[itemID].Count > 0)
+        {
+            GameObject particleEffect = particleEffects[itemID].Pop();
+            particleEffect.transform.position = position;
+            return particleEffect;
+        }
+        else
+        {
+            GameObject prefab = itemDatabase.GetParticleEffect(itemID);
+            prefab.GetComponent<ParticleEffectHandler>().objectPool = this;
+            Quaternion rotation = prefab.transform.rotation;
+            GameObject effect=Instantiate(prefab, position, rotation);
+            return effect;
+        }
+    }
+   
+    public void ReturnParticleEffectToPool(GameObject item, int itemID)
+    {
+        particleEffects[itemID].Push(item);
+    }
+    public GameObject GetRandomItemFromPool(Vector3 position)
+    {
+        int randomItemID = Random.Range(0, 4);
+        if (items[randomItemID].Count > 0)
+        {
+            GameObject item = items[randomItemID].Pop();
+            item.transform.position = position;
+            return item;
+        }
+        else
+        {
+            GameObject prefab = Instantiate(itemDatabase.GetBoardElement(randomItemID), position, Quaternion.identity);
+            return prefab;
+        }
+    }
+
+    public void ReturnItemToPool(GameObject item, int itemID)
+    {
+        items[itemID].Push(item);
+    }
+}
