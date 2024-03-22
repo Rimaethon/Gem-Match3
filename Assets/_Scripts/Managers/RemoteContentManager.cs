aaba9d914128ef40ee74c2daef6fb4c15603bab1
Yoldaş Ulaş <yutalas@gmail.com>
2024-03-22 12:20:24 +0300

    AWS SDK And Remote Content Manager Script added
    For making levels downloadable remotely I'm setting up an S3 bucket

---
+++ Assets/_Scripts/Managers/RemoteContentManager.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+namespace DefaultNamespace
+{
+    public class RemoteContentManager:MonoBehaviour
+    {
+        public IAmazonS3 S3Client;
+        
+        void Start()
+        {
+            AmazonS3Config s3Config = new AmazonS3Config();
+            s3Config.RegionEndpoint = Amazon.RegionEndpoint.USEast1;
+            
+           
+        }
+        
+        public async UniTask GetObjects()
+        {
+            GetObjectRequest request = new GetObjectRequest
+            {
+                BucketName = "match3contentbucket",
+                Key = "Level1.json"
+            };
+
+            S3Client.GetObjectAsync(request, (response) =>
+            {
+                using (Stream responseStream = response.Response.ResponseStream)
+                using (StreamReader reader = new StreamReader(responseStream))
+                {
+                    string responseBody = reader.ReadToEnd(); // Now you have the object data
+                    Debug.Log(responseBody);
+                }
+            });
+          
+        }
+        
+    }
+}
