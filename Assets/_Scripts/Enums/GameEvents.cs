6ce37d640ff3fe06a3c0c61b13eb448daba84729
Yoldaş Ulaş <yutalas@gmail.com>
2024-03-22 18:41:52 +0300

    UI Goal Scriptable Objects and Prefabs Added
    To have a generic structure that can show different types of goals in goal bar,  I used SO to store the varying data for each goal type

---
+++ Assets/_Scripts/Enums/GameEvents.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameEvents
+{
+    
+    OnMoveCountChanged,
+    OnLevelLoaded,
+    OnLevelCompleted,
+    OnLevelFailed,
+    OnSwipe,
+    OnTileSelected,
+    OnLevelButtonPressed,
+    OnLevelStart,
+    OnLevelEnd,
+    OnButtonClick,
+    OnDataInitialization,
+    OnGameStart,
+    OnGameSceneLoaded,
+    OnMenuSceneLoaded,
+    OnClick,
+    OnHorizontalMatch,
+    OnBomb,
+    OnTouch
+}
---
+++ Assets/_Scripts/Scriptable Objects/UIElementSO.cs
@@ -0,0 +1,11 @@
+﻿
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    [CreateAssetMenu(fileName = "UIElementSO", menuName = "Create UIElementSO")]
+    public class UIElementSO:AbstractItemDataSO
+    {
+    
+    }
+}
