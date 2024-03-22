a1b21f77267fbf1d43bf3e3ad8b0d24034c487b0
Yoldaş Ulaş <yutalas@gmail.com>
2024-03-22 11:54:11 +0300

    Grid Manager And Random Board Generator Script  Added
    To make testing easy I wrote a random board generator that fills the board with 1 swap matchable Items.

---
+++ Assets/_Scripts/Core/RandomBoardGenerator.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Profiling;
+using UnityEngine;
+using UnityEngine.Profiling;
+using UnityEngine.Timeline;
+using Random = Unity.Mathematics.Random;
+
+namespace _Scripts.Core
+{
+    // This class can also be adjusted for making levels harder with generating less one swap matchable doublets
+    public class RandomBoardGenerator
+    {
+        static readonly ProfilerMarker myMarker = new ProfilerMarker("GenerateRandomBoard");
+        [BurstCompile]
+        public int[,] GenerateRandomBoard(int width=8, int height=10,int minTypeID=0,int maxTypeID=4)
+        {
+            var job = new GenerateBoardJob
+            {
+                Width = width,
+                Height = height,
+                Board = new NativeArray<int>(width * height, Allocator.TempJob),
+                Random = new Random((uint)DateTime.Now.Ticks),
+                MinTypeID = minTypeID,
+                MaxTypeID = maxTypeID,
+                Marker = myMarker,
+            };
+
+            job.Schedule().Complete();
+
+            int[,] result = new int[width, height];
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    result[i, j] = job.Board[j * width + i];
+                }
+            }
+
+            job.Board.Dispose();
+            return result;
+        }
+
+        [BurstCompile]
+        struct GenerateBoardJob : IJob
+        {
+            public int Height;
+            public int Width;
+            public NativeArray<int> Board;
+            public Random Random;
+            public int MinTypeID;
+            public int MaxTypeID;
+            
+            public ProfilerMarker Marker;
+            public void Execute()
+            {
+                Marker.Begin();
+                do
+                {
+                    GenerateBoardWithNoTriplets(Width,Height, Random,MinTypeID,MaxTypeID, ref Board);
+                } while (!HasMatchableSwap(Board,Width,Height));
+                Marker.End();
+            }
+        }
+        
+        [BurstCompile]
+        private static void GenerateBoardWithNoTriplets(int width, int height, Random random,int minTypeID,int maxTypeID, ref NativeArray<int> board)
+        {
+            for (int x = 0; x < width; x++)
+            for (int y = 0; y < height; y++)
+            {
+                int randomIndex = random.NextInt(minTypeID, maxTypeID);
+                board[y * width + x] = randomIndex;
+
+                // Check for triplets and square matches and change the cell type if there is a match
+                while ((y >= 1 && x >= 1 && board[(y - 1) * width + x] == randomIndex && board[y * width + x - 1] == randomIndex &&
+                        board[(y - 1) * width + x - 1] == randomIndex) ||
+                       (x >= 2 && board[y * width + x - 1] == randomIndex && board[y * width + x - 2] == randomIndex) ||
+                       (y >= 2 && board[(y - 1) * width + x] == randomIndex && board[(y - 2) * width + x] == randomIndex))
+                {
+                    randomIndex = random.NextInt(minTypeID, maxTypeID);
+                    board[y * width + x] = randomIndex;
+                }
+            }
+        }
+
+        
+        
+        // Check if there is a matchable swap with swapping every two adjacent cells and checking for triplets
+        [BurstCompile]
+        private static bool HasMatchableSwap(NativeArray<int> board,int width, int height)
+        {
+            for (int y = 0; y < width; y++)
+            for (int x = 0; x < height; x++)
+            for (int i = 0; i < 4; i++)
+            {
+                //swap left right up down logic since there are 4 directions to check I'm iterating 4 times
+                int dx = i == 0 ? 0 : i == 1 ? 1 : i == 2 ? 0 : -1;
+                int dy = i == 0 ? 1 : i == 1 ? 0 : i == 2 ? -1 : 0;
+                int nx = x + dx;
+                int ny = y + dy;
+                
+                if (nx < 0 || nx >= width || ny < 0 || ny >= height) continue;
+                // Swap
+                int temp = board[y * width + x];
+                board[y * width + x] = board[ny * width + nx];
+                board[ny * width + nx] = temp;
+
+                // Check for triplet
+                if (HasTriplet(board, x, y, width,height) || HasTriplet(board, nx, ny,width, height))
+                {
+                    // Swap back
+                    board[ny * width + nx] = board[y * width + x];
+                    board[y * width + x] = temp;
+                    
+                    return true;
+                }
+
+                // Swap back
+                board[ny * width + nx] = board[y * width + x];
+                board[y * width + x] = temp;
+            }
+
+            return false;
+        }
+        
+ 
+    
+         
+        [BurstCompile]
+public static string HasShape(NativeArray<int> board, int x, int y, int width, int height)
+{
+    int value = board[y * width + x];
+
+    // Check for square
+    if (x < width - 1 && y < height - 1 && board[(y * width) + x + 1] == value && board[((y + 1) * width) + x] == value && board[((y + 1) * width) + x + 1] == value)
+    {
+        return "Square";
+    }
+
+    // Check for triplet
+    if ((x >= 2 && board[y * width + x - 1] == value && board[y * width + x - 2] == value) ||
+        (x < width - 2 && board[y * width + x + 1] == value && board[y * width + x + 2] == value) ||
+        (y >= 2 && board[(y - 1) * width + x] == value && board[(y - 2) * width + x] == value) ||
+        (y < height - 2 && board[(y + 1) * width + x] == value && board[(y + 2) * width + x] == value))
+    {
+        // Check for quadruplet
+        if ((x >= 3 && board[y * width + x - 3] == value) ||
+            (x < width - 3 && board[y * width + x + 3] == value) ||
+            (y >= 3 && board[(y - 3) * width + x] == value) ||
+            (y < height - 3 && board[(y + 3) * width + x] == value))
+        {
+            // Check for L shape
+            if ((x >= 2 && y < height - 2 && board[(y + 1) * width + x] == value && board[(y + 2) * width + x] == value) ||
+                (x < width - 2 && y < height - 2 && board[(y + 1) * width + x] == value && board[(y + 2) * width + x] == value) ||
+                (x >= 2 && y >= 2 && board[(y - 1) * width + x] == value && board[(y - 2) * width + x] == value) ||
+                (x < width - 2 && y >= 2 && board[(y - 1) * width + x] == value && board[(y - 2) * width + x] == value))
+            {
+                return "L";
+            }
+
+            // Check for T shape
+            if (x >= 1 && x < width - 1 && y < height - 2 && board[(y + 1) * width + x - 1] == value && board[(y + 1) * width + x + 1] == value)
+            {
+                return "T";
+            }
+
+            return "Quadruplet";
+        }
+
+        return "Triplet";
+    }
+
+    return "None";
+}
+
+        // Check if there is a triplet at the given cell
+        [BurstCompile]
+        private static bool HasTriplet(NativeArray<int> board, int x, int y, int width, int height)
+        {
+            int value = board[y * width + x];
+            return (x >= 2 && board[y * width + x - 1] == value && board[y * width + x - 2] == value) ||
+                   (x < width - 2 && board[y * width + x + 1] == value && board[y * width + x + 2] == value) ||
+                   (y >= 2 && board[(y - 1) * width + x] == value && board[(y - 2) * width + x] == value) ||
+                   (y < height - 2 && board[(y + 1) * width + x] == value && board[(y + 2) * width + x] == value);
+        }
+        
+        
+    }
+}
---
+++ Assets/_Scripts/Managers/GridManager.cs
@@ -0,0 +1,370 @@
+using System.Collections.Generic;
+using _Scripts.Core;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using Rimaethon.Scripts.Managers;
+using UnityEngine;
+using Sequence = DG.Tweening.Sequence;
+using Vector3Int = UnityEngine.Vector3Int;
+
+namespace Scripts
+{
+    public class GridManager : MonoBehaviour
+    {
+        
+        [SerializeField] private Grid grid;
+
+        //Pool is only needed for the grid so I'm passing it with serialized field
+        [SerializeField] private ObjectPool _objectPool;
+        private Camera _camera;
+        private bool _isLerping;
+        private int[,] gameBoard;
+        [SerializeField] private int width;
+        [SerializeField] private int height;
+        [SerializeField] private float lerpSpeed=0.2f;
+
+        private IItem[,] _tileItems;
+        //Im only using local position to 
+        private Vector2[,] _cellPositions;
+        
+        private HashSet<Vector3Int> lerpingItems=new HashSet<Vector3Int>();
+        private RandomBoardGenerator _randomBoardGenerator;
+        
+        
+        private void OnEnable()
+        {
+            EventManager.Instance.AddHandler<Vector3Int, Vector2>(GameEvents.OnSwipe, OnSwipe);
+            EventManager.Instance.AddHandler<Vector2>(GameEvents.OnClick, OnClick);
+            EventManager.Instance.AddHandler<List<Vector3Int>>(GameEvents.OnHorizontalMatch, HorizontalMatch);
+            EventManager.Instance.AddHandler<List<Vector3Int>>(GameEvents.OnBomb, BombMatch);
+            EventManager.Instance.AddHandler<Vector2>(GameEvents.OnTouch, OnTouch);
+        }
+
+        private void OnDisable()
+        {
+            if (EventManager.Instance == null) return;
+            EventManager.Instance.RemoveHandler<Vector3Int, Vector2>(GameEvents.OnSwipe, OnSwipe);
+            EventManager.Instance.RemoveHandler<Vector2>(GameEvents.OnClick, OnClick);
+            EventManager.Instance.RemoveHandler<List<Vector3Int>>(GameEvents.OnHorizontalMatch, HorizontalMatch);
+            EventManager.Instance.RemoveHandler<List<Vector3Int>>(GameEvents.OnBomb, BombMatch);
+            EventManager.Instance.RemoveHandler<Vector2>(GameEvents.OnTouch, OnTouch);
+        }
+
+        private async void HorizontalMatch(List<Vector3Int> elements)
+        {
+            await HandleMatchWithDelay(elements);
+        }
+        private async void BombMatch(List<Vector3Int> elements)
+        {
+             await HandleBombMatch(elements);
+        }
+
+        
+        private void OnTouch(Vector2 touchPos)
+        {
+            Vector3Int firstCellPos = grid.WorldToCell(touchPos);
+            if (IsInsideGrid(firstCellPos)&&_tileItems[firstCellPos.x, firstCellPos.y]!=null)
+            {
+                _tileItems[firstCellPos.x, firstCellPos.y].OnTouch();
+            }
+        }
+        
+        private bool IsInsideGrid(Vector3Int pos)
+        {
+            return pos.x >= 0 && pos.x < width && pos.y >= 0 && pos.y < height;
+        }
+    
+        private void Awake()
+        {
+
+            _camera = Camera.main;
+        }
+
+        private void Start()
+        {
+            Application.targetFrameRate = 60;
+            _randomBoardGenerator = new RandomBoardGenerator();
+            gameBoard = _randomBoardGenerator.GenerateRandomBoard(width, height);
+            Generate();
+        }
+
+        private void Generate()
+        {
+
+            _tileItems = new IItem[width, height];
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    
+                    _tileItems[i,j] =_objectPool.GetItemFromPool(gameBoard[i, j], grid.GetCellCenterWorld(new Vector3Int(i, j, 0))).GetComponent<IItem>();
+                    
+                    _tileItems[i, j].Transform.parent = transform;
+                }
+                
+
+            }
+          
+        }
+
+
+        private async void OnClick(Vector2 clickPos)
+        {
+            Vector3Int firstCellPos = grid.WorldToCell(clickPos);
+            if (!IsDraggable(firstCellPos))
+                return;
+            _tileItems[firstCellPos.x,firstCellPos.y].OnClick(_tileItems,new Vector2Int(firstCellPos.x,firstCellPos.y));
+            OnTouch(clickPos);
+        }
+
+        private bool isSwiping=false;
+        private async void  OnSwipe(Vector3Int direction, Vector2 clickPos)
+        {
+
+            if (isSwiping)
+            {
+                return;
+            }
+            Vector3Int firstCellPos = grid.WorldToCell(clickPos);
+            Vector3Int secondCellPos = firstCellPos + direction;
+          
+            if (!IsDraggable(firstCellPos) || !IsDraggable(secondCellPos))
+                return;
+            isSwiping = true;
+
+            Debug.Log(lerpingItems.Contains(firstCellPos));
+            lerpingItems.Add(firstCellPos);
+            lerpingItems.Add(secondCellPos);    
+            _tileItems[firstCellPos.x, firstCellPos.y].SortingOrder= 5;
+            _tileItems[secondCellPos.x, secondCellPos.y].SortingOrder = 4;
+            await ChangeItemPositions(firstCellPos, secondCellPos); 
+            bool pos2HasMatch = await HasMatch(firstCellPos);
+            bool pos1HasMatch = await HasMatch(secondCellPos);
+            if (!pos1HasMatch&& !pos2HasMatch)
+            {
+                await ChangeItemPositions(secondCellPos, firstCellPos); 
+            }
+            isSwiping = false;
+            OnTouch(clickPos);
+            
+        }
+        private bool IsDraggable(Vector3Int pos)
+        {
+            return  pos is { x: >= 0, y: >=0 } && pos.x<width && pos.y<height&&_tileItems[pos.x,pos.y]!=null&&_tileItems[pos.x, pos.y].IsDraggable&& !lerpingItems.Contains(pos)&&_tileItems[pos.x, pos.y].IsMovable;
+        }
+
+        private async UniTask ChangeItemPositions(Vector3Int pos1, Vector3Int pos2, float lerpSpeed = 0.15f)
+        {
+            Vector3 pos1InitialPosition = grid.GetCellCenterLocal(pos1);
+            Vector3 pos2InitialPosition = grid.GetCellCenterLocal(pos2);
+            Sequence sequence = DOTween.Sequence();
+            sequence.Insert(0,_tileItems[pos1.x, pos1.y].Transform.DOLocalMove(pos2InitialPosition, lerpSpeed));
+            sequence.Insert(0,_tileItems[pos2.x, pos2.y].Transform.DOLocalMove(pos1InitialPosition, lerpSpeed));
+
+            await sequence.AwaitForComplete();    
+            SwapItemsInArray(pos1, pos2);
+          
+           
+        }
+      
+        private void SwapItemsInArray(Vector3Int pos1, Vector3Int pos2)
+        {
+            (_tileItems[pos1.x, pos1.y], _tileItems[pos2.x, pos2.y]) = (_tileItems[pos2.x, pos2.y], _tileItems[pos1.x, pos1.y]);
+        }
+        
+    
+        public static List<Vector3Int> CheckHorizontalMatches(IItem[,] board, int itemX, int itemY, int width, int height)
+        {
+            List<Vector3Int> matchedPositions = new List<Vector3Int>();
+            IItem currentItem = board[itemX, itemY];
+
+            // Check to the right
+            for (int x = itemX; x < width; x++)
+            {
+                if (board[x, itemY] != null && board[x, itemY].ItemType == currentItem.ItemType)
+                {
+                    matchedPositions.Add(new Vector3Int(x, itemY, 0));
+                }
+                else
+                {
+                    break;
+                }
+            }
+            
+            // Check to the left
+            for (int x = itemX - 1; x >= 0; x--)
+            {
+                if (board[x, itemY] != null && board[x, itemY].ItemType == currentItem.ItemType)
+                {
+                    matchedPositions.Add(new Vector3Int(x, itemY, 0));
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return matchedPositions.Count >= 3 ? matchedPositions : new List<Vector3Int>();
+        }
+
+        public static List<Vector3Int> CheckVerticalMatches(IItem[,] board, int itemX, int itemY, int width, int height)
+        {
+            List<Vector3Int> matchedPositions = new List<Vector3Int>();
+            IItem currentItem = board[itemX, itemY];
+
+            // Check upwards
+            for (int y = itemY; y < height; y++)
+            {
+                if (board[itemX, y] != null && board[itemX, y].ItemType == currentItem.ItemType)
+                {
+                    matchedPositions.Add(new Vector3Int(itemX, y, 0));
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            // Check downwards
+            for (int y = itemY - 1; y >= 0; y--)
+            {
+                if (board[itemX, y] != null && board[itemX, y].ItemType == currentItem.ItemType)
+                {
+                    matchedPositions.Add(new Vector3Int(itemX, y, 0));
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return matchedPositions.Count >= 3 ? matchedPositions : new List<Vector3Int>();
+        }
+        private async UniTask<bool> HasMatch(Vector3Int pos)
+        {
+            List<Vector3Int> verticalMatches = CheckVerticalMatches(_tileItems, pos.x, pos.y, width, height);
+            List<Vector3Int> horizontalMatches = CheckHorizontalMatches(_tileItems, pos.x, pos.y, width, height);
+
+            if (Mathf.Max(verticalMatches.Count, horizontalMatches.Count) < 3)
+            {
+                lerpingItems.Remove(pos);
+                return false;
+            }
+            if(verticalMatches.Count>horizontalMatches.Count)
+            {
+                if (verticalMatches.Count == 5)
+                {
+                    verticalMatches.RemoveAt(0);
+                    _tileItems[pos.x, pos.y].OnMatch();
+                    _tileItems[pos.x,pos.y]=_objectPool.GetItemFromPool(5, grid.GetCellCenterWorld(pos)).GetComponent<IItem>();
+                    _tileItems[pos.x, pos.y].Transform.parent = transform;
+                    await HandleMatchWithDelay(verticalMatches);
+
+                    return true;
+                }else if (verticalMatches.Count == 4)
+                {
+                    verticalMatches.RemoveAt(0);
+                    _tileItems[pos.x, pos.y].OnMatch();
+                    //_tileItems[pos.x,pos.y]=_objectPool.GetItemFromPool(7, grid.GetCellCenterWorld(pos)).GetComponent<IItem>();
+                    _tileItems[pos.x,pos.y]=_objectPool.GetItemFromPool(5, grid.GetCellCenterWorld(pos)).GetComponent<IItem>();
+                    _tileItems[pos.x, pos.y].Transform.parent = transform;
+
+                    await HandleMatchWithDelay(verticalMatches);
+
+                    return true;
+                }
+                else if(verticalMatches.Count==3)
+                {
+                    await HandleMatchWithDelay(verticalMatches);
+                    return true;
+                }
+
+            }
+            
+        
+            if (horizontalMatches.Count == 5)
+            {
+                horizontalMatches.RemoveAt(0);
+                _tileItems[pos.x, pos.y].OnMatch();
+                //_tileItems[pos.x, pos.y] = _objectPool.GetItemFromPool(5, grid.GetCellCenterWorld(pos)).GetComponent<IItem>();
+                _tileItems[pos.x,pos.y]=_objectPool.GetItemFromPool(5, grid.GetCellCenterWorld(pos)).GetComponent<IItem>();
+                _tileItems[pos.x, pos.y].Transform.parent = transform;
+
+                await HandleMatchWithDelay(horizontalMatches);
+
+                return true;
+            }else if (horizontalMatches.Count == 4)
+            {
+                horizontalMatches.RemoveAt(0);
+                _tileItems[pos.x, pos.y].OnMatch();
+               // _tileItems[pos.x,pos.y]=_objectPool.GetItemFromPool(6, grid.GetCellCenterWorld(pos)).GetComponent<IItem>();
+               _tileItems[pos.x,pos.y]=_objectPool.GetItemFromPool(5, grid.GetCellCenterWorld(pos)).GetComponent<IItem>();
+               _tileItems[pos.x, pos.y].Transform.parent = transform;
+
+               await HandleMatchWithDelay(horizontalMatches);
+                return true;
+            }
+            _tileItems[pos.x, pos.y].Transform.localPosition=grid.GetCellCenterLocal(pos);
+            
+            await  HandleMatchWithDelay(horizontalMatches);
+            return true;
+            
+        }
+        
+    
+        
+     
+
+        private bool isBoardDirty;
+        private int[] missingItems=new int[8];
+        [SerializeField]private int[] y = new int[8];
+        
+        private async UniTask HandleMatchWithDelay(List<Vector3Int> matchedItems, float delayBetweenMatches = 0.1f,float delayToFillBoard=0.1f)
+        {
+            foreach (var item in matchedItems)
+            {
+                if (_tileItems[item.x, item.y] == null)
+                {
+                    return;
+                }
+                _tileItems[item.x, item.y].OnMatch();
+                _objectPool.GetParticleEffectFromPool(_tileItems[item.x, item.y].ItemType, grid.GetCellCenterWorld(item));
+                await UniTask.Delay((int)(delayBetweenMatches * 1000));
+            }
+            await UniTask.Delay((int)(delayToFillBoard * 1000));
+            foreach (var item in matchedItems)
+            {
+                missingItems[item.x]++;
+                _tileItems[item.x, item.y] = null;
+
+            }
+        }
+        
+  
+        private async UniTask HandleBombMatch(List<Vector3Int> matchedItems)
+        {
+            
+            foreach (var item in matchedItems)
+            {
+                _tileItems[item.x, item.y].OnMatch();
+                _objectPool.GetParticleEffectFromPool(_tileItems[item.x, item.y].ItemType, grid.GetCellCenterWorld(item));
+                _tileItems[item.x, item.y] = null;
+
+            }
+
+            await UniTask.Delay(400);
+            foreach (var item in matchedItems)
+            {
+                missingItems[item.x]++;
+            }
+            
+        }
+     
+
+ 
+        
+     
+
+     
+    }
+}
