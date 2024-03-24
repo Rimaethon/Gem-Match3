abc07b44f02020d6ed5a382796ee406cf251c67a
Yoldaş Ulaş <yutalas@gmail.com>
2024-03-24 14:42:01 +0300

    Base Classes for Scriptable Objects And Board Elements Added

---
+++ Assets/_Scripts/Scriptable Objects/AbstractItemData.cs
@@ -0,0 +1,10 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public abstract class AbstractItemDataSO:SerializedScriptableObject
+    {
+        public GameObject ItemPrefab;
+    }
+}
