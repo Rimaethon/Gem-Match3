8f4ff084dac5830d65541d29f81594619a9197ba
Yoldaş Ulaş <yutalas@gmail.com>
2024-03-26 23:51:47 +0300

    ArrayExtensions.cs Created
    Grid Manager was filled with duplicate methods that are taking entire line so I wrote a wrapper class to make it look more understandable.

---
+++ Assets/_Scripts/Utility/ArrayExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Unity.Burst.Intrinsics;
+using UnityEngine;
+
+namespace _Scripts.Utility
+{
+    public static class ArrayExtensions
+    {
+        public static IItem GetItem(this IItem[,] array, Vector3Int position)
+        {
+            return array[position.x, position.y];
+        }
+        public static float GetDistance(this IItem[,] array,Vector2[,] cellPositions,int x, int y)
+        {
+            return Vector2.Distance(array[x,y].Transform.localPosition, cellPositions[x,y]);
+        }
+   
+        public static int GetTypeID(this IItem[,] array, Vector3Int position)
+        {
+            return array[position.x, position.y].ItemType;
+        }
+
+        public static void Set(this IItem[,] array, Vector3Int position, IItem value)
+        {
+            array[position.x, position.y]= value;
+        }
+        
+        public static Vector3 GetLocal(this Grid grid, Vector3Int position)
+        {
+            return grid.GetCellCenterWorld(new Vector3Int(position.x, position.y, position.z));
+        }
+        
+        
+    }
+}
