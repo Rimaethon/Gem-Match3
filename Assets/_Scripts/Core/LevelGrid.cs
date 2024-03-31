0fb3563e862ea1350adce82958235b6786b19e0f
Yoldaş Ulaş <yutalas@gmail.com>
2024-03-31 14:11:21 +0300

    LevelGrid.cs Created
    Since a level can have multiple boards but only one grid, grid logic needed to be extracted from board class. Also grid related extensions are moved to here.

---
+++ Assets/_Scripts/Core/LevelGrid.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using Rimaethon.Scripts.Utility;
+using UnityEngine;
+
+public class LevelGrid : Singleton<LevelGrid>
+{
+    public static Grid Grid => _grid;
+    private static Grid _grid;
+    protected override void Awake()
+    {
+        base.Awake();
+        _grid=gameObject.GetComponent<Grid>();
+    }
+    
+    public Vector3 GetCellCenterWorld(Vector2Int position)
+    {
+        return _grid.GetCellCenterWorld(new Vector3Int(position.x,position.y,0));
+    }
+    public  Vector2 GetCellCenterLocalVector2(Vector2Int position)
+    {
+        return new Vector2(_grid.GetCellCenterLocal(new Vector3Int(position.x,position.y,0)).x,_grid.GetCellCenterLocal(new Vector3Int(position.x,position.y,0)).y);
+    }
+
+    public  Vector2Int WorldToCellVector2Int( Vector2 pos)
+    {
+        return new Vector2Int(_grid.WorldToCell(pos).x,_grid.WorldToCell(pos).y);
+    }
+    public  Vector2Int WorldToCellVector2Int( Vector3 pos)
+    {
+        return new Vector2Int(_grid.WorldToCell(pos).x,_grid.WorldToCell(pos).y);
+    }
+
+}
