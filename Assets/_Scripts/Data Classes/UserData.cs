abe481f26d6727f2c7e7b7e913d70146165c6093
Yoldaş Ulaş <yutalas@gmail.com>
2024-04-01 13:41:28 +0300

    UserData class and its json serialized instance are created
    I tried to keep it simple for the sake of development time since it would need much more variables to accomodate all the data in a real product

---
+++ Assets/_Scripts/Data Classes/UserData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Sirenix.Serialization;
+
+namespace Data
+{
+    [Serializable]
+    public class UserData
+    {
+        public int currentLevel=1;
+        public int coinCount=1000;
+        public int heartCount=5;
+        public int starCount=0;
+        public Dictionary<int, int> BoosterCounts;
+    }
+}
