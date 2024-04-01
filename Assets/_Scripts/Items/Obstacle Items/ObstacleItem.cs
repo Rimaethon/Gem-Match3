aa57e5d56c082043ec55b0aa88e07cf89edb37b7
Yoldaş Ulaş <yutalas@gmail.com>
2024-04-01 11:20:14 +0300

    ObstacleItem.cs Created
    This class will be the base class for obstacles to inherit.

---
+++ Assets/_Scripts/Items/Obstacle Items/ObstacleItem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using Rimaethon.Scripts.Managers;
+using UnityEngine;
+
+namespace Scripts
+{
+    public class ObstacleItem:ItemBase
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+            isFallAble = false;
+            _isMatchable = false;
+            _isSwappable = false;
+        }
+
+        public override void OnExplode()
+        {
+            if(IsExploding)
+                return;
+            _isExploding= true;
+            ObjectPool.Instance.GetItemParticleEffect(_itemID, Transform.position);
+            ObjectPool.Instance.ReturnItem(Item, _itemID);
+            EventManager.Instance.Broadcast<Vector2Int>(GameEvents.AddItemToRemoveFromBoard, _position);
+
+        }
+    
+        public override void OnClick(Board board, Vector2Int pos)
+        {
+            if (IsMoving || IsExploding||_isClicked)
+                return;
+            Debug.Log("MatchItem Clicked"+pos);
+            transform.DOPunchRotation(new Vector3(0, 0, 20), 0.5f).OnComplete(() => {_isClicked = false;});            
+        }
+        
+
+    }
+}
