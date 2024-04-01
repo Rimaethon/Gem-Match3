67e6b9a8f7e841df66fd21dc7252079890d61432
Yoldaş Ulaş <yutalas@gmail.com>
2024-04-01 12:48:59 +0300

    VerticalRocketBooster.cs Created

---
+++ Assets/_Scripts/Items/BoosterItems/VerticalRocketBooster.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using _Scripts.Utility;
+using Rimaethon.Scripts.Managers;
+using Scripts.BoosterActions;
+using UnityEngine;
+
+namespace Scripts
+{
+    public class VerticalRocketBooster:ItemBase
+    {
+        VerticalRocketBoosterAction _action = new VerticalRocketBoosterAction();
+        
+        private void OnEnable()
+        {
+            transform.localScale = Vector3.one;
+            _isBooster = true;
+            _isSwappable = true;
+            _isMatchable = false;
+            isFallAble = true;
+            IsActive = true;
+            _isExploding = false;
+            _isHighlightAble = true;
+            IsMoving = false;
+            IsMatching = false;
+
+        }
+
+
+ 
+        public override void OnClick(Board board, Vector2Int pos)
+        {
+            if(IsMoving||IsExploding)
+                return;
+            OnExplode();
+        }
+    
+        
+        public override void OnExplode()
+        {
+            if(IsExploding)
+                return;
+            _isExploding= true;
+            _action.Execute(Board,Position,_itemID);
+            OnRemove();
+        }
+        public override void OnRemove()
+        {
+            ObjectPool.Instance.ReturnItem(Item, ItemID);
+            EventManager.Instance.Broadcast(GameEvents.AddItemToRemoveFromBoard, Position);
+        }
+
+
+    }
+}
