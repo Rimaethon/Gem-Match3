d2a5af7d3f305e406dc416b46dbd3ec0558b68c2
Yoldaş Ulaş <yutalas@gmail.com>
2024-04-02 12:22:28 +0300

    Level Generator Class Updated
    It gets the job done for now but Its needed to replace it with a better system such as a tilemap based level creator.

--- Assets/_Scripts/Node/LevelGenerator.cs
+++ Assets/_Scripts/Node/LevelGenerator.cs
@@ -1,45 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using DefaultNamespace;
 using Sirenix.OdinInspector;
+using Sirenix.Serialization;
 using UnityEditor;
 using UnityEditor.Sprites;
 using UnityEngine;
+using SerializationUtility = Sirenix.Serialization.SerializationUtility;
 
 namespace Scripts.Node
 {
     public class LevelGenerator : MonoBehaviour
     {
-        public bool IfExistsDonotGenerate;
-        public String path="Assets/Resources/Levels/Level ";
-        public int levelID=1;
-        public int backgroundID=0;
-        
-        
-        [HorizontalGroup]
-        public List<int> GoalSpriteIds;
-        [HorizontalGroup]
-        public List<int> GoalCounts;
-       
-        public int moveCount=1;
-        [SerializeField] public ItemDatabaseSO boardElements;
-        
-        [ShowInInspector][TableMatrix(DrawElementMethod = "DrawElement",RowHeight = 90)]
-        public int[,] BoardElementIDs=new int[8,10];
-        private LevelData _levelData= new LevelData();
-       
-        
-        
-        private int DrawElement(Rect rect, int value)
+        [SerializeField] private bool ifExistsDonotGenerate;
+        [SerializeField] private string path = "Assets/Resources/Levels/Level ";
+        [SerializeField] private int levelID = 1;
+        [SerializeField] private int backgroundID;
+        [ShowInInspector] [TableMatrix(DrawElementMethod = "DrawElement", RowHeight = 90)]
+        [SerializeField] private int[,] goalIds=new int[4,1];
+        [SerializeField] private int moveCount = 1;
+        [SerializeField] private ItemDatabaseSO itemDataBase;
+        private readonly LevelData _levelData = new();
+        private readonly List<int> _goalCounts = new();
+        [ShowInInspector] [TableMatrix(DrawElementMethod = "DrawElement", RowHeight = 90)]
+        public int[,] BoardElementIDs = new int[8, 10];
+
+        private int DrawGoals(Rect rect, int value)
         {
-            if(boardElements.BoardElements.Count==0)
+            if (itemDataBase.NormalItems.Count == 0)
             {
                 Debug.LogError("Board Elements are empty");
                 return value;
             }
 
-            int val = boardElements.BoardElements.Count;
-            if(Event.current.type==EventType.MouseDown&&rect.Contains(Event.current.mousePosition))
+            var val = itemDataBase.NormalItems.Count;
+            if (Event.current.type == EventType.MouseDown && rect.Contains(Event.current.mousePosition))
             {
                 value = (value + 1) % val;
                 GUI.changed = true;
@@ -47,32 +43,59 @@ namespace Scripts.Node
             }
 
             // Increase the size of the rectangle
-            
             // Draw the texture with a larger scale
             EditorGUI.DrawPreviewTexture(rect, SpriteUtility.GetSpriteTexture
-                (boardElements.BoardElements[value].ItemPrefab.GetComponent<SpriteRenderer>().sprite,false),
+                    (itemDataBase.NormalItems[value].ItemPrefab.GetComponent<SpriteRenderer>().sprite, false),
                 null, ScaleMode.ScaleToFit);
             return value;
         }
-        
-        
-        
-        
-        
-        
+
+        private void Reset()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                goalIds[i, 0] = -1;
+            }
+        }
+
+        private int DrawElement(Rect rect, int value)
+        {
+            
+            if (itemDataBase.NormalItems.Count == 0)
+            {
+                Debug.LogError("Board Elements are empty");
+                return value;
+            }
+
+            var val = itemDataBase.NormalItems.Count+1;
+            if (Event.current.type == EventType.MouseDown && rect.Contains(Event.current.mousePosition))
+            {
+                value = ((value + 1) % val)-1;
+                GUI.changed = true;
+                Event.current.Use();
+            }
+
+            if (value != -1)
+            {
+                EditorGUI.DrawPreviewTexture(rect, SpriteUtility.GetSpriteTexture
+                        (itemDataBase.NormalItems[value].ItemPrefab.GetComponent<SpriteRenderer>().sprite, false),
+                    null, ScaleMode.ScaleToFit);
+            }
+           
+            return value;
+        }
+
+
         [Button]
         private void GenerateNonRectangularGrid()
         {
-            if (IfExistsDonotGenerate && System.IO.File.Exists(path + levelID + ".json"))
+            if (ifExistsDonotGenerate && File.Exists(path + levelID + ".json"))
             {
                 Debug.LogError($"Level {levelID} already exists");
                 return;
             }
-            if(GoalCounts.Count!=GoalSpriteIds.Count || GoalSpriteIds.Count>4)
-            {
-                Debug.LogError("Goal Sprite IDs and Goal Counts must have the same length and must be less than 5");
-                return;
-            }
+
+         
 
             if (moveCount <= 0)
             {
@@ -85,20 +108,9 @@ namespace Scripts.Node
                 Debug.LogError("Board Element IDs must have at least 1 row and 1 column");
                 return;
             }
-            
-            _levelData.LevelID = levelID;
-            _levelData.BackgroundID = backgroundID;
-            _levelData.GoalSpriteIDs = GoalSpriteIds.ToArray();
-            _levelData.GoalCounts = GoalCounts.ToArray();
-            _levelData.MoveCount = moveCount;
-            _levelData.BoardElementIDs = BoardElementIDs;            
-            SaveToJson();
+
         }
-        
-        private void SaveToJson()
-        {
-            byte[] serializedData = Sirenix.Serialization.SerializationUtility.SerializeValue(_levelData, Sirenix.Serialization.DataFormat.JSON);
-            System.IO.File.WriteAllBytes(path + levelID + ".json", serializedData);
-        }
+
+       
     }
 }
