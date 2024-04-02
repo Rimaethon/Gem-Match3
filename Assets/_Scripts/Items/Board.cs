e35d31674dae36f44e6bdcf69341192dabdb39fc
Yoldaş Ulaş <yutalas@gmail.com>
2024-04-02 19:09:40 +0300

    Board And Cell Classes are created
    Handling everything from IItem interface was a bad idea. Now Cell class holds cell related data which was handled with ItemBase class before

---
+++ Assets/_Scripts/Items/Board.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using _Scripts.Data_Classes;
+using UnityEngine;
+
+namespace Scripts
+{
+    [Serializable]
+    public class Board
+    {
+        public Cell[,] Cells;
+        public int Width => _width;
+        public int Height=> _height;
+        private int _width;
+        private int _height;
+        public Board(int width, int height)
+        {
+            _width= width;
+            _height = height;
+            Cells = new Cell[width, height];
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    Cells[x, y] = new Cell(new Vector2Int(x,y));
+                }
+            }
+  
+        }
+       
+    }
+}
---
+++ Assets/_Scripts/Items/Cell.cs
@@ -0,0 +1,58 @@
+﻿
+using UnityEngine;
+
+namespace Scripts
+{
+    public class Cell
+    {
+        public Cell(Vector2Int cellPosition,IItem item=null,IItem underLayItem=null,IItem overLayItem=null,bool isNotInBoard=false)
+        {
+            CellPosition = cellPosition;
+            SetItem(item);
+            UnderLayItem = underLayItem;
+            OverLayItem = overLayItem;
+            IsNotInBoard = isNotInBoard;
+        }
+        public bool IsNotInBoard;
+        public bool HasItem => Item != null;
+        public bool HasUnderLayItem => UnderLayItem != null;
+        public bool HasOverLayItem => OverLayItem != null;
+        public Vector2Int CellPosition;
+        public bool IsGettingFilled => _isGettingFilled;
+        public bool IsGettingEmptied => _isGettingEmptied;
+        public bool IsLocked => _isLocked;
+        public IItem Item=>_item;
+        private IItem _item;
+        public IItem UnderLayItem;
+        public IItem OverLayItem;
+        private bool _isLocked;
+        private bool _isGettingFilled ;
+        private bool _isGettingEmptied;
+        
+        public void SetItem(IItem item)
+        {
+            _item = item;
+            if (Item != null)
+            {
+                Item.Position = CellPosition;
+            }
+          Debug.Log("Item set to "+item+" for cell "+CellPosition.x+" "+CellPosition.y);
+        }
+        
+        public void SetIsGettingFilled(bool value)
+        {
+            _isGettingFilled = value;
+           Debug.Log("Is getting filled set to "+value+" for cell "+CellPosition.x+" "+CellPosition.y);
+        }
+        public void SetIsGettingEmptied(bool value)
+        {
+            _isGettingEmptied = value;
+       Debug.Log("Is getting Emptied set to "+value+" for cell "+CellPosition.x+" "+CellPosition.y);
+        }
+        public void SetIsLocked(bool value)
+        {
+            _isLocked = value;
+       Debug.Log("Is getting Locked set to "+value+" for cell "+CellPosition.x+" "+CellPosition.y);
+        }
+    }
+}
