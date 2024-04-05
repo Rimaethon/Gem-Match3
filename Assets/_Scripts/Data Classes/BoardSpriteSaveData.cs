f26a6cef0a660980bc56e459a5b4b9ff953fd55f
Yoldaş Ulaş <yutalas@gmail.com>
2024-04-05 15:42:08 +0300

    BoardSpriteSaveData.cs And BoardCreator Prefab are created
    Now I can create custom boards and tie them with  related data which makes it really easy to create and use boards.

---
+++ Assets/_Scripts/Data Classes/BoardSpriteSaveData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace _Scripts.Data_Classes
+{
+    public class BoardSpriteSaveData
+    {
+        public int Width;
+        public int Height;
+        public HashSet<Vector2Int> BlankCells;
+        public Sprite Sprite;
+        public BoardSpriteSaveData(Sprite sprite, int width, int height, HashSet<Vector2Int> blankCells)
+        {
+            Sprite = sprite;
+            Width = width;
+            Height = height;
+            BlankCells = blankCells;
+        }
+        
+    }
+}
