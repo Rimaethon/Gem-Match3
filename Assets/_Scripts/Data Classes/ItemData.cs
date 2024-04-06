80a45fecfb17fb89abf78985204836900bb3b9ae
Yoldaş Ulaş <yutalas@gmail.com>
2024-04-06 16:19:11 +0300

    ItemData.cs created and ItemDatabaseSO.asset updated
    Now I can easily add new items to ItemDatabase SO without dealing with 100+ scriptable objects

---
+++ Assets/_Scripts/Data Classes/ItemData.cs
@@ -0,0 +1,15 @@
+﻿using Scripts;
+using UnityEngine;
+
+namespace _Scripts.Data_Classes
+{
+    public class ItemData
+    {
+        public Sprite ItemSprite;
+        public int ItemHealth=1;
+        public GameObject ItemPrefab;
+        public GameObject ItemParticleEffect;
+        public IItemAction ItemAction;
+        
+    }
+}
--- Assets/_Scripts/Scriptable Objects/ItemDatabaseSO.cs
+++ Assets/_Scripts/Scriptable Objects/ItemDatabaseSO.cs
@@ -1,25 +1,80 @@
+using System;
 using System.Collections.Generic;
-using DefaultNamespace;
+using _Scripts.Data_Classes;
+using Scripts;
+using Scripts.BoosterActions;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "ItemDatabase", menuName = "Create ItemDatabase")]
 public class ItemDatabaseSO : SerializedScriptableObject
 {
-    [DictionaryDrawerSettings(KeyLabel = "Item Label", ValueLabel = "Item SO")]
-    public Dictionary<int, AbstractItemDataSO> BoardElements = new Dictionary<int, AbstractItemDataSO>();
-    public GameObject GetBoardElement(int id)
+    [DictionaryDrawerSettings(KeyLabel = "Item ID", ValueLabel = "Item Data")]
+    public GameObject boosterCreationEffect;
+    public GameObject matchEffect;
+    public Dictionary<int, ItemData> NormalItems = new Dictionary<int, ItemData>();
+    public Dictionary<int, ItemData> Boosters = new Dictionary<int, ItemData>();
+    public List<Tuple<int,int,IItemMerge>> BoosterMergeAction = new List<Tuple<int, int,IItemMerge>>();
+    public Dictionary<int, BoardSpriteSaveData> Boards = new Dictionary<int, BoardSpriteSaveData>();
+    
+    
+    [Button("Set Item ID's")]
+    public void Initialize()
     {
-         BoardElements[id].ItemPrefab.GetComponent<IItem>().ItemType = id;
-       
-        return BoardElements[id].ItemPrefab;
+        foreach (KeyValuePair<int,ItemData> item in NormalItems)
+        {
+            if(item.Value.ItemPrefab!=null)
+                item.Value.ItemPrefab.GetComponent<IItem>().ItemID = item.Key;
+            if (item.Value.ItemParticleEffect != null)
+                item.Value.ItemParticleEffect.GetComponent<ItemParticleEffect>().ItemID = item.Key;
+     
+        }
+        foreach (KeyValuePair<int,ItemData> item in Boosters)
+        {
+            if(item.Value.ItemPrefab!=null)
+                item.Value.ItemPrefab.GetComponent<IItem>().ItemID = item.Key;
+            if (item.Value.ItemParticleEffect != null)
+                item.Value.ItemParticleEffect.GetComponent<BoosterParticleEffect>().ItemID = item.Key;
+        }
+        
+        Debug.Log("Item ID's are set.");
+    }
+    public GameObject GetNormalItem(int id)
+    {
+        return NormalItems[id].ItemPrefab;
+    }
+    public GameObject GetNormalItemParticleEffect(int id)
+    {
+        return NormalItems[id].ItemParticleEffect;
+    }
+    public IItemAction GetNormalItemAction(int id)
+    {
+        return NormalItems[id].ItemAction;
+    }
+    public IItemAction GetBoosterItemAction(int id)
+    {
+        return Boosters[id].ItemAction;
+    }
+    public GameObject GetBooster(int id)
+    {
+        return Boosters[id].ItemPrefab;
+    }
+    public GameObject GetBoosterParticleEffect(int id)
+    {
+        return Boosters[id].ItemParticleEffect; 
+    }
+    public GameObject GetBoosterCreationEffect()
+    {
+        return boosterCreationEffect;
+    }
+    public GameObject GetMatchEffect()
+    {
+        return matchEffect;
+    }
+    public BoardSpriteSaveData GetBoardSpriteData(int id)
+    {
+        return Boards[id];
+    }
+    
 
-    }
-    public GameObject GetParticleEffect(int id)
-    {
-        ParticleEffects[id].ItemPrefab.GetComponent<ParticleEffectHandler>().particleEffectID = id;
-        return ParticleEffects[id].ItemPrefab;
-    }
-    public Dictionary<int, AbstractItemDataSO> UIElements = new Dictionary<int, AbstractItemDataSO>();
-    public Dictionary<int, AbstractItemDataSO> ParticleEffects = new Dictionary<int, AbstractItemDataSO>();
 }
