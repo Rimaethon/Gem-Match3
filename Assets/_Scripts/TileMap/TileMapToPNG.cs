c0448e93932b0130e1de8478f2c761cedcd1ad7b
Yoldaş Ulaş <yutalas@gmail.com>
2024-04-07 18:35:11 +0300

    TileMapToPNG.cs Updated And TilemapConverter Editor script deleted

--- Assets/_Scripts/TileMap/TileMapToPNG.cs
+++ Assets/_Scripts/TileMap/TileMapToPNG.cs
@@ -1,5 +1,6 @@
 #if UNITY_EDITOR
 using System.IO;
+using Sirenix.OdinInspector;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Tilemaps;
@@ -10,20 +11,41 @@ namespace Scripts.Node
 {
     public class TileMapToPNG : MonoBehaviour
     {
-        private Tilemap _tileMap;
-        public Texture2D img;
+        [SerializeField] private Tilemap tileMap;
+        private Texture2D _imageTexture;
         private float _pixelSize;
+        [SerializeField] private string pathToSave = "Assets/Art/Boards/";
+        [SerializeField] private string spriteName;
 
-        public void Pack()
+
+        [Button]
+        public void ResetTilemap()
         {
-            _tileMap = GetComponent<Tilemap>();
+            tileMap.ClearAllTiles();
+        }
+        [Button]
+        public void ExportAsPng()
+        {
+            Pack();
+            byte[] bytes = _imageTexture.EncodeToPNG();
+            var dirPath = Application.dataPath + pathToSave;
+            if (!Directory.Exists(dirPath))
+            {
+                Directory.CreateDirectory(dirPath);
+            }
+            File.WriteAllBytes(dirPath + spriteName + ".png", bytes);
+            _imageTexture = null;
+            AssetDatabase.Refresh();
+        }
+        private void Pack()
+        {
 
             int spriteWidth=0;
             int spriteHeight=0;
-            _tileMap.CompressBounds();    
-            foreach (Vector3Int pos in _tileMap.cellBounds.allPositionsWithin)
+            tileMap.CompressBounds();    
+            foreach (Vector3Int pos in tileMap.cellBounds.allPositionsWithin)
             {
-                Sprite sprite = _tileMap.GetSprite(pos);
+                Sprite sprite = tileMap.GetSprite(pos);
                 if (sprite == null) continue;
                 spriteWidth = (int)sprite.textureRect.width;
                 spriteHeight = (int)sprite.textureRect.height;
@@ -43,7 +65,7 @@ namespace Scripts.Node
                 return;
             }
 
-            Texture2D createdImage = new Texture2D(spriteWidth * _tileMap.cellBounds.size.x, spriteHeight * _tileMap.cellBounds.size.y);
+            Texture2D createdImage = new Texture2D(spriteWidth * tileMap.cellBounds.size.x, spriteHeight * tileMap.cellBounds.size.y);
 
             Color[] clearPixels = new Color[createdImage.width * createdImage.height];
             for (int i = 0; i < clearPixels.Length; i++)
@@ -53,33 +75,22 @@ namespace Scripts.Node
             createdImage.SetPixels(clearPixels);
             createdImage.Apply();
 
-            foreach (Vector3Int pos in _tileMap.cellBounds.allPositionsWithin)
+            foreach (Vector3Int pos in tileMap.cellBounds.allPositionsWithin)
             {
-                Sprite sprite = _tileMap.GetSprite(pos);
+                Sprite sprite = tileMap.GetSprite(pos);
 
                 if (sprite != null)
                 {
                     Color[] pixels = sprite.texture.GetPixels((int)sprite.textureRect.x, (int)sprite.textureRect.y, spriteWidth, spriteHeight);
-                    createdImage.SetPixels((pos.x - _tileMap.cellBounds.min.x) * spriteWidth, (pos.y - _tileMap.cellBounds.min.y) * spriteHeight, spriteWidth, spriteHeight, pixels);
+                    createdImage.SetPixels((pos.x - tileMap.cellBounds.min.x) * spriteWidth, (pos.y - tileMap.cellBounds.min.y) * spriteHeight, spriteWidth, spriteHeight, pixels);
                 }
             }
 
             createdImage.Apply();
-            img = createdImage;
+            _imageTexture = createdImage;
         }
 
-        public void ExportAsPng(string textureName)
-        {
-            byte[] bytes = img.EncodeToPNG();
-            var dirPath = Application.dataPath + "/Exported Tilemaps/";
-            if (!Directory.Exists(dirPath))
-            {
-                Directory.CreateDirectory(dirPath);
-            }
-            File.WriteAllBytes(dirPath + textureName + ".png", bytes);
-            img = null;
-            AssetDatabase.Refresh();
-        }
+       
     }
 }
 #endif
