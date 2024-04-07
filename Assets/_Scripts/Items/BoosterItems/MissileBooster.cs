9827718b5317fddc333c498e18f6417307083aaf
Yoldaş Ulaş <yutalas@gmail.com>
2024-04-07 12:53:28 +0300

    MissileBooster.cs and Missile Particle Effects are created
    Sending Flames to 4 directions with 1 particle system was really hard but I didn't wanted to create 4 flame effects for each direction and after spending couple hours I manage to do it as I wanted.

---
+++ Assets/_Scripts/Items/BoosterItems/MissileBooster.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using _Scripts.Managers;
+using _Scripts.Utility;
+using Rimaethon.Scripts.Managers;
+using Scripts.BoosterActions;
+using UnityEngine;
+
+namespace Scripts
+{
+    public class MissileBooster:ItemBase
+    {
+        private MissileBoosterAction _action;
+
+        private void OnEnable()
+        {
+            transform.localScale = Vector3.one;
+            _isBooster = true;
+            _isSwappable = true;
+            _isMatchable = false;
+            isFallAble = true;
+            IsActive = true;
+            _isExploding = false;
+            _isHighlightAble = true;
+            IsMoving = false;
+        }
+
+    
+        public override void OnClick(Board board, Vector2Int pos)
+        {
+            if(IsMoving||IsExploding)
+                return;
+            OnExplode();
+        }
+        public override void OnExplode()
+        {
+            if(IsExploding)
+                return;
+            _isExploding= true;
+            Vector2Int goalPos = LevelManager.Instance.GetRandomGoalPos();
+            if (goalPos is { x: -1, y: -1 })
+            {
+                return;
+            }
+            _action = new MissileBoosterAction(goalPos);
+            _action.Execute(Board, Position, _itemID);
+            OnRemove();
+        }
+        public override void OnRemove()
+        {
+            ObjectPool.Instance.ReturnItem(Item, ItemID);
+            EventManager.Instance.Broadcast(GameEvents.AddItemToRemoveFromBoard, Position);
+        }
+    }
+}
