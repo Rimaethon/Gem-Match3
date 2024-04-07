16931bc062ef00dcc34d4e02d9cad62ffa9d92ea
Yoldaş Ulaş <yutalas@gmail.com>
2024-04-07 09:43:47 +0300

    MatchData.cs and MatchType.cs are created
    MatchData.cs is for holding the matched items in an array with a MatchType enum and MatchType.cs is for determining whether to spawn an booster- Triple match or explode the items with enums.

---
+++ Assets/_Scripts/Managers/Matching/MatchData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using _Scripts.Managers.Matching;
+using Scripts;
+using UnityEngine;
+
+namespace _Scripts.Core
+{
+    public class MatchData
+    {
+        public Match[] Matches=new Match[14];
+        public MatchType MatchType=MatchType.None;
+    }
+}
---
+++ Assets/_Scripts/Managers/Matching/MatchType.cs
@@ -0,0 +1,15 @@
+﻿
+namespace _Scripts.Core
+{
+    public enum MatchType
+    {
+        TNT=100,
+        HorizontalRocket=101,
+        VerticalRocket=102,
+        LightBall=103,
+        Missile=104,
+        Normal,
+        SingleExplosion,
+        None
+    }
+}
