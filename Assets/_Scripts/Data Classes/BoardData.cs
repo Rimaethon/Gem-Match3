10c2155613ed46c7c6c8f23a5e435fdb05cc6319
Yoldaş Ulaş <yutalas@gmail.com>
2024-04-08 14:21:43 +0300

    BoardData.cs Created and Old Blue Item sprites are changed with new ones
    BoardData.cs will be used for creating level boards and serializing them to json

---
+++ Assets/_Scripts/Data Classes/BoardData.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _Scripts.Data_Classes
+{
+    public class BoardData
+    {
+        public Vector3 BoardPosition;
+        public int[,] NormalItemIds;
+        public Dictionary<Vector2Int,int> UnderlayItemIds;
+        public Dictionary<Vector2Int,int> OverlayItemIds;
+        public int  BoardSpriteID;
+        public BoardData(int boardSpriteID,Vector3 boardPosition,int[,] normalItemIds, Dictionary<Vector2Int,int> underlayItemIds, Dictionary<Vector2Int,int> overlayItemIds)
+        {
+            BoardSpriteID= boardSpriteID;
+            BoardPosition = boardPosition;
+            NormalItemIds = normalItemIds;
+            UnderlayItemIds = underlayItemIds;
+            OverlayItemIds = overlayItemIds;
+        }
+    }
+}
