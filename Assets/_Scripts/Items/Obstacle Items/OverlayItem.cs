3e789043f8fc4a732538d142c76770e3c5d4af33
Yoldaş Ulaş <yutalas@gmail.com>
2024-04-09 16:19:38 +0300

    OverlayItem.cs created
    This will be used as base class for all the overlays which will probably be goals in a level.

---
+++ Assets/_Scripts/Items/Obstacle Items/OverlayItem.cs
@@ -0,0 +1,35 @@
+﻿using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using Rimaethon.Scripts.Managers;
+using UnityEngine;
+
+namespace Scripts
+{
+    public class OverlayItem:ItemBase
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+            isFallAble = false;
+            _isMatchable = false;
+            _isSwappable = false;
+            
+        }
+
+        public override void OnExplode()
+        {
+            if(IsExploding)
+                return;
+            _isExploding= true;
+            ExplodeObject().Forget();
+        }
+        private async UniTask ExplodeObject()
+        {
+            ObjectPool.Instance.GetItemParticleEffect(_itemID, Transform.position);
+            await UniTask.Delay(200);
+        }
+       
+    
+
+    }
+}
