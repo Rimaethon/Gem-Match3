392f1f672ab6e21f1988927775df4fd6874d7775
Yoldaş Ulaş <yutalas@gmail.com>
2024-04-09 13:19:06 +0300

    GoalSaveData.cs and BoardDataCreator.cs are created for Level Creating tool
    BoardDataCreator will combine board data and since there can be multiple boards on a single level it would be better to make a LevelCreator that combines all the boards

---
+++ Assets/_Scripts/Data Classes/GoalSaveData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace _Scripts.Data_Classes
+{
+    public class GoalSaveData
+    {
+        public int[] GoalIDs;
+        public int[] GoalCounts;
+        public GoalSaveData(int[] goalIDs, int[] goalCounts)
+        {
+            GoalIDs = goalIDs;
+            GoalCounts = goalCounts;
+        }
+    }
+}
---
+++ Assets/_Scripts/Utility/BoardDataCreator.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using _Scripts.Data_Classes;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace _Scripts.Utility
+{
+    
+    [RequireComponent(typeof(SpriteRenderer))][ExecuteInEditMode]
+    public class BoardDataCreator:MonoBehaviour
+    {
+        public int BoardID;
+        private HashSet<Vector2Int> _blankCells;
+        private SpriteRenderer _spriteRenderer;
+        [SerializeField] private ItemDatabaseSO itemDatabase;
+        [ShowInInspector][ReadOnly]
+        private int _boardWidth;
+        [ShowInInspector][ReadOnly]
+        private int _boardHeight;
+        [SerializeField] ItemIDMatrixCreator normalItemIDMatrixCreator;
+        [SerializeField] ItemIDMatrixCreator underlayItemIDMatrixCreator;
+        [SerializeField] ItemIDMatrixCreator overlayItemIDMatrixCreator;
+        private BoardSpriteSaveData _boardSpriteSaveData;
+        private BoardData _board;
+        private void Awake()
+        {
+            if(itemDatabase==null)
+                throw new Exception("Item Database is null");
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            GetBoardSpriteData();
+            InitializeItemMatrices();
+        }
+        [Button]
+        public void GetBoardSpriteData()
+        {
+            _boardSpriteSaveData= itemDatabase.GetBoardSpriteData(BoardID);
+            _spriteRenderer.sprite=_boardSpriteSaveData.Sprite;
+            _boardWidth = _boardSpriteSaveData.Width;
+            _boardHeight = _boardSpriteSaveData.Height;
+            _blankCells = _boardSpriteSaveData.BlankCells;
+        }
+        [Button]
+        public void ResetAllTilemaps()
+        {
+            normalItemIDMatrixCreator.ResetTilemap();
+            underlayItemIDMatrixCreator.ResetTilemap();
+            overlayItemIDMatrixCreator.ResetTilemap();
+        }
+        [Button]
+        public void InitializeItemMatrices()
+        {
+            normalItemIDMatrixCreator.InitializeItemIDMatrix(_boardWidth, _boardHeight);
+            underlayItemIDMatrixCreator.InitializeItemIDMatrix(_boardWidth, _boardHeight);
+            overlayItemIDMatrixCreator.InitializeItemIDMatrix(_boardWidth, _boardHeight);
+        }
+        public BoardData CreateBoardData()
+        {
+            
+            int[,] normalItemIds = normalItemIDMatrixCreator.ItemIDMatrix;
+            Dictionary<Vector2Int,int> underlayItemIds= new Dictionary<Vector2Int,int>();
+            Dictionary<Vector2Int,int> overlayItemIds= new Dictionary<Vector2Int,int>();
+            CreateDictionaryFromMatrix(normalItemIDMatrixCreator.ItemIDMatrix,underlayItemIds);
+            CreateDictionaryFromMatrix(underlayItemIDMatrixCreator.ItemIDMatrix,underlayItemIds);
+            _board = new BoardData(BoardID,Vector3.zero, normalItemIds, underlayItemIds, overlayItemIds);
+            return _board;
+        }
+        private void CreateDictionaryFromMatrix(int[,] matrix, Dictionary<Vector2Int,int> dictionary)
+        {
+            dictionary.Clear();
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] != -1)
+                    {
+                        dictionary.Add( new Vector2Int(i,j),matrix[i, j]);
+                    }
+                }
+            }
+        }
+
+    }
+}
