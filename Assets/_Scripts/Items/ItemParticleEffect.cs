52a075e7ca9757663768c2e7290afb99fa1f49e5
Yoldaş Ulaş <yutalas@gmail.com>
2024-04-12 17:29:49 +0300

    ItemParticleEffect.cs created and Old HorizontalCracker.cs deleted
    It was not working well and It got useless after architecture change

---
+++ Assets/_Scripts/Items/ItemParticleEffect.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Scripts
+{
+    public class ItemParticleEffect:MonoBehaviour
+    {
+        public int ItemID;
+
+        private void OnDisable()
+        {
+            if(ObjectPool.Instance==null)
+                return;
+            Debug.Log("Returning Particle Effect"+gameObject.name);
+            ObjectPool.Instance.ReturnParticleEffect(gameObject, ItemID);
+        }
+    }
+}
