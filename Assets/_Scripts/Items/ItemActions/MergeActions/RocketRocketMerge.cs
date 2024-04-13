1a00444a1825bcf69eece56db26e775f48ceb8b8
Yoldaş Ulaş <yutalas@gmail.com>
2024-04-13 10:46:42 +0300

    RocketRocketMerge.cs created and Hammer Booster Sprite added

---
+++ Assets/_Scripts/Items/ItemActions/MergeActions/RocketRocketMerge.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Scripts.BoosterActions.MergeActions
+{
+    public class RocketRocketMerge :IItemMerge
+    {
+
+        public void Execute(IItem item, IItem item2)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
