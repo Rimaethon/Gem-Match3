6df09dae9757065f75332ba4f6085f12d3f5ee23
Yoldaş Ulaş <yutalas@gmail.com>
2024-04-19 09:47:07 +0300

    Main Menu Scene Updated with new UI Images and LightGrassUnderLayItem.cs is created

---
+++ Assets/_Scripts/Items/Obstacle Items/LightGrassUnderLayItem.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Scripts
+{
+    //Should this class inherit from ItemBase or not ? I'm asking this question for every item class I create. Maybe all of us are classes that inherit from ItemBase in a parallel universe.
+    public class LightGrassUnderLayItem:UnderlayItem
+    {
+      
+    }
+}
