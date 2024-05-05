95b8c6e577bcdfbd84b6f63673e8c93d92e6e236
Yoldaş Ulaş <yutalas@gmail.com>
2024-05-05 10:27:00 +0300

    SpawnHandler.cs Updated
    BoardInstance is now passed as parameter for determining the parent gameobject of the spawned object also AddItem Method name changed to AddBooster for clarification since that method is only used for spawning boosters and while writing this I realized that some items may have that ability in the future :/

--- Assets/_Scripts/Core/SpawnHandler.cs
+++ Assets/_Scripts/Core/SpawnHandler.cs
@@ -15,18 +15,21 @@ namespace _Scripts.Core
         private readonly Board _board;
         private readonly int _width;
         private readonly int _height;
-        public SpawnHandler(Board board)
+        private GameObject _boardInstance;
+        public SpawnHandler(GameObject boardInstance,Board board)
         {
             _board = board;
             _width = _board.Width;
             _height = _board.Height;
-            EventManager.Instance.AddHandler<Vector2Int,int>(GameEvents.AddItemToAddToBoard, AddItemToAddToBoard);
+            _boardInstance = boardInstance;
+            EventManager.Instance.AddHandler<Vector2Int,int>(GameEvents.AddItemToAddToBoard, AddBoosterToAddToTheBoard);
+
         }
         
-        private void AddItemToAddToBoard(Vector2Int itemPos,int itemID)
+        //I hope someday I don't be so brain dead  and give better names to these methods
+        private void AddBoosterToAddToTheBoard(Vector2Int itemPos,int itemID)
         {
-            _itemsToAddToBoard.Add(itemPos,itemID);
-            Debug.Log("Item Added to board"+itemPos);
+            if(!_itemsToAddToBoard.TryAdd(itemPos, itemID)) return;
         }
         public void HandleBoosterSpawn()
         {
@@ -35,16 +38,23 @@ namespace _Scripts.Core
             {
                 _board.GetCell(itemData.Key).SetIsLocked(true);
                 _itemsToAddToBoard.Remove(itemData.Key);
-                _board.GetCell(itemData.Key).SetItem(ObjectPool.Instance.GetBoosterGameObject(itemData.Value, LevelGrid.Instance.GetCellCenterWorld(itemData.Key), _board));
-                _board.GetItem(itemData.Key).Transform.parent = LevelGrid.Grid.transform;
-                _board.GetItem(itemData.Key).FallSpeed = 2;
-
-                ObjectPool.Instance.GetBoosterCreationEffect(LevelGrid.Instance.GetCellCenterWorld(itemData.Key));
-                _board.GetItem(itemData.Key).Transform.DOPunchScale(Vector3.one * 1.2f, 0.3f,0,0).onComplete += () =>
-                {
-                    _board.GetCell(itemData.Key).SetIsLocked(false);
-                };
                 
+                IBoardItem boardItem = ObjectPool.Instance.GetBoosterItem(itemData.Value, LevelGrid.Instance.GetCellCenterWorld(itemData.Key), _board);
+                boardItem.Transform.parent = _boardInstance.transform;
+                if (_board.GetCell(itemData.Key).HasItem)
+                {
+                    ObjectPool.Instance.ReturnItem(_board.GetItem(itemData.Key), _board.GetItem(itemData.Key).ItemID);
+                }
+                _board.GetCell(itemData.Key).SetIsGettingEmptied(false);
+                _board.GetCell(itemData.Key).SetIsGettingFilled(false);
+                _board.GetCell(itemData.Key).SetItem(boardItem);
+                _board.GetItem(itemData.Key).TargetToMove = itemData.Key;
+                _board.GetItem(itemData.Key).IsMoving = true;
+                _board.GetCell(itemData.Key).SetIsLocked(false);
+                boardItem.Transform.DOScale(Vector3.one * 1.2f, 0.15f)
+                    .SetLoops(2, LoopType.Yoyo)
+                    .SetUpdate(UpdateType.Fixed);
+
             }
             _itemsToAddToBoardThisFrame.Clear();
         }
@@ -55,11 +65,12 @@ namespace _Scripts.Core
                 //           Debug.Log("Checking spawn"+x+" "+_height+" "+_board.GetCell(x,_height-1).HasItem+" "+_board.GetCell(x,_height-1).IsLocked);
                 if (_board.GetCell(x, _height - 1).HasItem||_board.GetCell(x,_height-1).IsLocked) continue;
                 int randomType = Random.Range(0, 4);
-                _board.GetCell(x,_height-1).SetItem(ObjectPool.Instance.GetItemGameObject(randomType, LevelGrid.Grid.GetCellCenterWorld(new Vector3Int(x, _height, 0)),_board));
-                _board.GetItem(x, _height - 1).Transform.parent = LevelGrid.Grid.transform;
+                _board.GetCell(x,_height-1).SetItem(ObjectPool.Instance.GetItem(randomType, LevelGrid.Grid.GetCellCenterWorld(new Vector3Int(x, _height, 0)),_board));
+                _board.GetItem(x, _height - 1).Transform.parent = _boardInstance.transform;
                 _board.GetItem(x, _height - 1).TargetToMove = new Vector2Int(x, _height - 1);
                  _board.GetItem(x,_height-1).IsMoving = true;
-                _board.GetItem(x,_height-1).FallSpeed = 2;
+                _board.GetCell(x,_height-1).SetIsGettingEmptied(false);
+                _board.GetCell(x,_height-1).SetIsGettingFilled(false);
             
             }
         }
