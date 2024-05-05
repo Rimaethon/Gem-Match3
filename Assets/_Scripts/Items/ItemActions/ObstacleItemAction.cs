d03d4539846e8bacb0d5c978ee428a3652886337
Yoldaş Ulaş <yutalas@gmail.com>
2024-05-05 13:21:06 +0300

    ObstacleItemAction.cs created
    It just explodes also this class can be used as a base class for items such as drill

---
+++ Assets/_Scripts/Items/ItemActions/ObstacleItemAction.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace Scripts.BoosterActions
+{
+    public class ObstacleItemAction : IItemAction
+    {
+        private readonly Board _board;
+        private readonly IBoardItem _boardItem;
+        private readonly float _explodeTime;
+        private float _counter;
+        public bool IsFinished { get; set; }
+
+        public ObstacleItemAction(Board board, IBoardItem boardItem, float explodeTime, bool isMatch)
+        {
+            _boardItem = boardItem;
+            _board = board;
+            _explodeTime = explodeTime;
+        }
+
+        public void InitializeAction()
+        {
+            ObjectPool.Instance.GetItemParticleEffect(_boardItem.ItemID, _boardItem.Transform.position);
+        }
+
+        public void Execute()
+        {
+            Explode();
+        }
+
+
+        private void Explode()
+        {
+            if (_counter < _explodeTime)
+            {
+                _counter += Time.deltaTime;
+                return;
+            }
+
+            IsFinished = true;
+        }
+    }
+}
