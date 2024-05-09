987c701586d041fe03400248594f8769ac66ce67
Yoldaş Ulaş <yutalas@gmail.com>
2024-05-09 13:04:24 +0300

    CanvasChangerButton.cs created And LevelButton.cs is updated

---
+++ Assets/_Scripts/UI/Buttons/CanvasChangerButton.cs
@@ -0,0 +1,18 @@
+﻿using DG.Tweening;
+using Rimaethon.Runtime.UI;
+using UnityEngine;
+
+public class CanvasChangerButton : UIButton
+{
+    
+    [SerializeField] private Canvas _canvasToClose;
+    [SerializeField] private Canvas _canvasToOpen;
+
+    protected override void DoOnClick()
+    {
+        Debug.Log("Clicked");
+        base.DoOnClick();
+        _canvasToClose.enabled = false;
+        _canvasToOpen.enabled = true;
+    }
+}
--- Assets/_Scripts/UI/Buttons/LevelButton.cs
+++ Assets/_Scripts/UI/Buttons/LevelButton.cs
@@ -1,49 +1,37 @@
-﻿using System;
-using Data;
-using DG.Tweening;
+﻿using DG.Tweening;
+using Rimaethon.Runtime.UI;
 using Rimaethon.Scripts.Managers;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace Rimaethon.Runtime.UI
+namespace _Scripts.UI.Buttons
 {
-    public class LevelButton: UIButton
+    public class LevelButton:UIButton
     {
         [SerializeField] TextMeshProUGUI levelText;
         [SerializeField] private Sprite greyButtonImage;
-        
-        private Button _button;
         private int _levelIndex=1;
         private const string LevelName = "Level ";
         private bool _isLevelExist;
 
         private void Start()
         {
-            _button = GetComponent<Button>();
-            if (levelText == null || _button == null)
-            {
-                Debug.LogError("Level Button is missing components");
-                return;                
-            }
             _levelIndex = SaveManager.Instance.GetLevelIndex();
+            _isLevelExist = SaveManager.Instance.HasNewLevel();
             levelText.text = LevelName+_levelIndex;
-            _isLevelExist = SaveManager.Instance.DoesLevelExist(_levelIndex);
-            if (!_isLevelExist)
-            {
-                _button.GetComponent<Image>().sprite = greyButtonImage;
-                levelText.color = Color.grey;
-            }
+            if (_isLevelExist) return;
+            Button.GetComponent<Image>().sprite = greyButtonImage;
+            levelText.color = Color.grey;
         }
-
-   
         protected override void DoOnClick()
         {
+            Debug.Log("Level Button Clicked");
             if(!_isLevelExist)
                 return;
+            base.DoOnClick();
+            
             EventManager.Instance.Broadcast(GameEvents.OnLevelButtonPressed, _levelIndex);
-
         }
-      
     }
 }
