366e90b80a9f9895c2c5ba779d9b77ce8afd643f
Yoldaş Ulaş <yutalas@gmail.com>
2024-05-10 14:55:21 +0300

    Input Manager Updated and Cannon In Game,CannonBall and Cannon UI Sprites Added

--- Assets/_Scripts/Input System/InputManager.cs
+++ Assets/_Scripts/Input System/InputManager.cs
@@ -2,66 +2,82 @@
 using Cysharp.Threading.Tasks;
 using Rimaethon.Scripts.Managers;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 namespace Scripts
 {
     public class InputManager: MonoBehaviour
     {
-        private PlayerInputs _playerInput;
+        [SerializeField] private GameObject _eventSystem;
+        private PlayerInputs _playerInputs;
+        private PlayerInput _playerInput;
         private Camera _mainCamera;
         private Vector2 _clickPos;
         private Vector2 _dragPos;
         private bool _isDragging=true;
         private bool _isBoardLocked;
         private int _booardLockCount; 
+        private int _playerInputLockCount;
         private void Awake()
         {
-            _playerInput = new PlayerInputs();
+            _playerInputs = new PlayerInputs();
+            _playerInput = GetComponent<PlayerInput>();
             _mainCamera = Camera.main;
         }
         private void OnEnable()
         {
-            _playerInput.Enable();
-            _playerInput.Player.Click.started +=OnTouch;
-            _playerInput.Player.Click.canceled += OnRelease;
-            _playerInput.Player.Drag.performed+= OnDrag;
-            EventManager.Instance.AddHandler(GameEvents.OnBoardLock, OnBoardLock);
-            EventManager.Instance.AddHandler(GameEvents.OnBoardUnlock, OnBoardUnlock);
+            _playerInputs.Enable();
+            _playerInputs.Player.Click.started +=OnTouch;
+            _playerInputs.Player.Click.canceled += OnRelease;
+            _playerInputs.Player.Drag.performed+= OnDrag;
+
+            EventManager.Instance.AddHandler(GameEvents.OnPlayerInputLock, OnPlayerInputLock);
+            EventManager.Instance.AddHandler(GameEvents.OnPlayerInputUnlock, OnPlayerInputUnlock);
         }
 
-      
-        private void OnDisable()
+        private void OnPlayerInputLock()
         {
-            _playerInput.Disable();
-            _playerInput.Player.Click.started -=OnTouch;
-            _playerInput.Player.Drag.performed-= OnDrag;
-            _playerInput.Player.Click.canceled -= OnRelease;
-            if (EventManager.Instance == null)
-                return;
-            EventManager.Instance.RemoveHandler(GameEvents.OnBoardLock, OnBoardLock);
-            EventManager.Instance.RemoveHandler(GameEvents.OnBoardUnlock, OnBoardUnlock);
+         _playerInputLockCount++;
+         _playerInputs.Disable();
+         _playerInput.enabled = false;
+         _eventSystem.SetActive(false);
+         _isBoardLocked = true;
         }
-
-        private void OnBoardLock()
+        private void OnPlayerInputUnlock()
         {
-            _booardLockCount++;
-            _isBoardLocked = true;
-        }
-
-        private void OnBoardUnlock()
-        {
-            _booardLockCount--;
-            if (_booardLockCount == 0)
+            _playerInputLockCount--;
+            if (_playerInputLockCount <= 0)
             {
+                _playerInputs.Enable();
+                _playerInput.enabled = true;
+                _eventSystem.SetActive(true);
                 _isBoardLocked = false;
             }
         }
+
+
+        private void OnDisable()
+        {
+            _playerInputs.Disable();
+            _playerInputs.Player.Click.started -=OnTouch;
+            _playerInputs.Player.Drag.performed-= OnDrag;
+            _playerInputs.Player.Click.canceled -= OnRelease;
+            if (EventManager.Instance == null)
+                return;
+
+            EventManager.Instance.RemoveHandler(GameEvents.OnPlayerInputLock, OnPlayerInputLock);
+            EventManager.Instance.RemoveHandler(GameEvents.OnPlayerInputUnlock, OnPlayerInputUnlock);
+        }
+
+
  
         private void OnTouch(InputAction.CallbackContext context)
         {
-            if (_isBoardLocked) return;
-            _clickPos = _mainCamera.ScreenToWorldPoint(_playerInput.Player.Drag.ReadValue<Vector2>());
+            _clickPos = _mainCamera.ScreenToWorldPoint(_playerInputs.Player.Drag.ReadValue<Vector2>());
+            EventManager.Instance.Broadcast(GameEvents.OnScreenTouch, _clickPos);
+            if (_isBoardLocked)
+                return;
             EventManager.Instance.Broadcast(GameEvents.OnTouch, _clickPos);
             _isDragging = true;
         }
@@ -84,7 +100,7 @@ namespace Scripts
         {
             if (_isBoardLocked) return;
             if (!_isDragging) return;
-            _dragPos = _mainCamera.ScreenToWorldPoint(_playerInput.Player.Drag.ReadValue<Vector2>());
+            _dragPos = _mainCamera.ScreenToWorldPoint(_playerInputs.Player.Drag.ReadValue<Vector2>());
             float xDiff = _dragPos.x- _clickPos.x  ;
             float yDiff =_dragPos.y-_clickPos.y ;
             if(Mathf.Abs(yDiff) > Mathf.Abs(xDiff) && Mathf.Abs(yDiff) > 0.1f)
