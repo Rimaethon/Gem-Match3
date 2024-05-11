d626db26fa5a5c5ac2ab07466f891bf65083da58
Yoldaş Ulaş <yutalas@gmail.com>
2024-05-11 08:44:06 +0300

    UIBoosterButton.cs created
    This is a data class which will generalize the button related data so that I don't have to create button data for each booster button.

---
+++ Assets/_Scripts/UI/Buttons/UIBoosterButton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Rimaethon.Runtime.UI
+{
+    [Serializable]
+    public class UIBoosterButton
+    {
+        public int itemID;
+        public TextMeshProUGUI boosterCounter;
+        public Button boosterButton;
+        public Image boosterBackground;
+        public GameObject unClickedCounter;
+        //This will be used as background particle effect in In Game Power Up Panel
+        public GameObject clickedCounter;
+        public GameObject unlimitedCounter;
+        public bool isClicked;
+        public RectTransform boosterTransform;
+        public GameObject boosterPanel;
+    }
+}
