4acc82d98549f7d04aa4e74403e4dede72565946
Yoldaş Ulaş <yutalas@gmail.com>
2024-05-14 09:38:58 +0300

    LıghtBall Booster Action Updated
    This actually took longer than excepted and it would probably be better to commit sooner. Now it can handle booster creation and its logic of choosing things is more reliable.

--- Assets/_Scripts/Items/ItemActions/LightBallBoosterAction.cs
+++ Assets/_Scripts/Items/ItemActions/LightBallBoosterAction.cs
@@ -1,170 +1,277 @@
-﻿using _Scripts.Core;
-using _Scripts.Managers.Matching;
+﻿using System.Collections.Generic;
+using System.Linq;
+using _Scripts.Managers;
 using _Scripts.Utility;
-using Cysharp.Threading.Tasks;
 using Rimaethon.Scripts.Managers;
 using UnityEngine;
 
 namespace Scripts.BoosterActions
 {
-    enum LightBallState
+    //Make LightBall,it would be fun they said.
+    //https://youtu.be/juvV9BRF3hI?t=217 as can be seen here when  LB-LB merge destroys everything the other Lightballs get destroyed because there is no item to match.
+    public class LightBallBoosterAction : IItemAction
     {
-        Search,
-        Animate,
-        Finish
-    }
-    public class LightBallBoosterAction :IItemAction
-    {
-        public bool IsFinished=>_isFinished;
-        private bool _isFinished;
-        private readonly LineRenderer _lineRenderer;
-        private readonly float _searchTime ;
-        private float _counter= 0.0f;
-        private readonly float _lerpSpeed ;
-        private readonly GameObject _itemParticleEffect;
-        private readonly GameObject _lightBallExplosionParticle;
-        private readonly GameObject _lightBallSprite;
-        private readonly Vector3 _lightBallPosition;
+        private const float ExplosionTime = 0.3f;
+        public bool IsFinished { get; set; }
+
         private readonly Board _board;
-        private readonly int _matchingItemID;
-        private readonly Vector2Int _position;
+        private readonly int _boosterID;
+        public readonly HashSet<Vector2Int> ItemsToExplode=new HashSet<Vector2Int>();
+        private readonly float _lerpTime;
         private readonly int _lightBallID;
-        private MatchData matchData;
-        private int _matchIndex = 0;
-        private LightBallState _state;
-        private IItem _itemToSendRay;
-        private float _lineLerpTime;
-        
-        public LightBallBoosterAction(Board board,int lightBallID,int matchingItemID, Vector2Int pos, float searchTime, float lerpSpeed)
+        private readonly Vector3 _lightBallPosition;
+        private readonly int _matchingItemID;
+        private readonly float _noTargetThreshold;
+        private readonly Vector2Int _position;
+        private readonly bool _shouldSpawnBooster;
+        private bool _isBoosterSpawned;
+        private float _finishCounter;
+        private GameObject _lightBallParticleEffectInstance;
+        private GameObject _lightBallExplosionParticles;
+        private GameObject _chargingParticleEffects;
+        private GameObject _lightBallSprite;
+        private List<LightBallRay> _targetedRays=new List<LightBallRay>();
+        private List<LightBallRay> _unTargetedRays=new List<LightBallRay>();
+        private int _targetCount;
+        private int _initialWaitForFrames;
+        private AudioSource _audioSource;
+        public float _noTargetTime=0.2f;
+        private float _noTargetCounter;
+        private Queue<Vector2Int> _rayDestinationQueue = new Queue<Vector2Int>();
+        private float searchWaitTime = 0.1f;
+        private float searchWaitCounter = 0.1f;
+        private bool isExplosionInitiated ;
+
+        public LightBallBoosterAction(Board board, int lightBallID, int matchingItemID, Vector2Int pos,
+            float noTargetThreshold, float lerpTime, bool shouldSpawnBooster = false, int boosterID = -1)
         {
             _board = board;
             _lightBallID = lightBallID;
             _matchingItemID = matchingItemID;
             _position = pos;
-            _searchTime = searchTime;
-            _lerpSpeed = lerpSpeed;
-            matchData = new MatchData();
-            matchData.MatchType = MatchType.SingleExplosion;
-            matchData.Matches = new Match[30];
+            _noTargetThreshold = noTargetThreshold;
+            _lerpTime = lerpTime;
+            _shouldSpawnBooster = shouldSpawnBooster;
+            _boosterID = boosterID;
+            _targetCount = 0;
+            _initialWaitForFrames = 10;
+            _finishCounter = 0;
+            _isBoosterSpawned= false;
             _lightBallPosition = LevelGrid.Instance.GetCellCenterWorld(pos);
-            _itemParticleEffect = ObjectPool.Instance.GetBoosterParticleEffect(_lightBallID, _lightBallPosition);
-            EventManager.Instance.Broadcast(GameEvents.OnBoardLock);
-            _lightBallExplosionParticle = _itemParticleEffect.GetComponent<LightBallParticleEffect>().lightBallExplosionParticle;
-            _lightBallSprite = _itemParticleEffect.GetComponent<LightBallParticleEffect>().lightBallSprite;
-            _lineRenderer = _itemParticleEffect.GetComponent<LineRenderer>();
-            _board.GetCell(pos).SetIsLocked( true);
-            HighlightLightBall();
-            
         }
-      
-       
-
-        private void HighlightLightBall()
+        public void InitializeAction()
         {
-            MaterialPropertyBlock materialPropertyBlock = new MaterialPropertyBlock();
+          _audioSource=AudioManager.Instance.PlaySFX(SFXClips.LightBallPoweringEffect,true);
+            SetupParticleEffects();
+            _board.GetCell(_position).SetIsLocked(true);
+            _lightBallSprite.SetActive(true);
+            _chargingParticleEffects.SetActive(true);
+            HighlightLightBall(true);
+            Search();
+            foreach (var ray in _unTargetedRays)
+            {
+                ray._lightBallBoosterAction= this;
+            }
+        }
+        private void SetupParticleEffects()
+        {
+            _lightBallParticleEffectInstance = ObjectPool.Instance.GetBoosterParticleEffect(_lightBallID, _lightBallPosition);
+            EventManager.Instance.Broadcast(GameEvents.OnBoardLock);
+            _lightBallExplosionParticles =
+                _lightBallParticleEffectInstance.GetComponent<LightBallParticleEffect>().lightBallExplosionParticle;
+            _chargingParticleEffects =_lightBallParticleEffectInstance.GetComponent<LightBallParticleEffect>().chargingParticleEffects;
+            _lightBallSprite = _lightBallParticleEffectInstance.GetComponent<LightBallParticleEffect>().lightBallSprite;
+            _unTargetedRays = _lightBallParticleEffectInstance.GetComponent<LightBallParticleEffect>().lightBallRays;
+            _chargingParticleEffects.SetActive(true);
+        }
+        private void HighlightLightBall(bool highlight)
+        {
+            var materialPropertyBlock = new MaterialPropertyBlock();
             _lightBallSprite.GetComponent<SpriteRenderer>().GetPropertyBlock(materialPropertyBlock);
-            materialPropertyBlock.SetFloat("_IsOutlineEnabled",1f);
+            materialPropertyBlock.SetFloat("_IsOutlineEnabled", highlight ? 1 : 0);
             _lightBallSprite.GetComponent<SpriteRenderer>().SetPropertyBlock(materialPropertyBlock);
         }
-        
+        private void TryGiveTargetToRays()
+        {
+            if (searchWaitCounter < searchWaitTime)
+            {
+                searchWaitCounter += Time.fixedDeltaTime;
+                return;
+            }
+            if (_rayDestinationQueue.Count == 0||_unTargetedRays.Count==0)
+            {
+                return;
+            }
+            searchWaitCounter = 0;
+            var target = _rayDestinationQueue.Dequeue();
+            _unTargetedRays[^1].SetTarget(_board,_shouldSpawnBooster,_boosterID,target);
+            _targetedRays.Add(_unTargetedRays[^1]);
+            _unTargetedRays.RemoveAt(_unTargetedRays.Count - 1);
+        }
         public void Execute()
         {
-            switch (_state)
+            Debug.Log("LightBallBoosterAction: Execute"+_initialWaitForFrames);
+            if(_initialWaitForFrames>0)
             {
-                case LightBallState.Search:
-                    Search();
-                    break;
-                case LightBallState.Animate:
-                    AnimateRay(_itemToSendRay);
-                    break;
-                case LightBallState.Finish:
+                _initialWaitForFrames--;
+                return;
+            }
+            TryGiveTargetToRays();
+            AnimateAndSearchRays();
+            
+            if (_rayDestinationQueue.Count == 0 && _targetedRays.Count == 0)
+            {
+                if(_shouldSpawnBooster)
+                {
                     FinishAction();
-                    break;
-                
+                }
+                else if (_noTargetCounter < _noTargetTime)
+                {
+                    _noTargetCounter += Time.fixedDeltaTime;
+                    Search();
+                }
+                else
+                {
+                    FinishAction();
+                }
             }
         }
-
+        private void AnimateAndSearchRays()
+        {
+            Debug.Log("Animating Ray"+_targetedRays.Count);
+            foreach (var ray in _targetedRays)
+            {
+                Debug.Log("Animating Ray"+ray.hasTarget);
+                if (ray.hasTarget)
+                {
+                    ray.AnimateRay();
+                }
+                else
+                {
+                    Debug.Log("Ray has no target");
+                    _unTargetedRays.Add(ray);
+                }
+            }
+            foreach (var ray in _unTargetedRays)
+            {
+                _targetedRays.Remove(ray);
+            }
+        }
         private void Search()
         {
-            while (_counter<_searchTime)
+            if (_shouldSpawnBooster)
             {
-                _counter += Time.fixedDeltaTime;
-                if (!TryGetItemToMatch(_board, _matchingItemID))
+                HashSet<Vector2Int> randomPositions = LevelManager.Instance.GetRandomSpawnablePos(10);
+                Debug.Log("RandomPositions"+randomPositions.Count);
+                foreach (var position in randomPositions)
                 {
-                    _state = LightBallState.Finish;
-                    return;
+                    Debug.Log("RandomPosition"+position);
+                    _rayDestinationQueue.Enqueue(position);
                 }
-                matchData.Matches[_matchIndex].Pos=_itemToSendRay.Position;
-                matchData.Matches[_matchIndex].IsMatch= true;
-                _matchIndex++;
-                _itemToSendRay.IsMatching = true;
-                _state = LightBallState.Animate;                
-               return;
+                ItemsToExplode.UnionWith(randomPositions);
             }
-        
+            else
+            {
+                TryGetItemPositionForRay(_matchingItemID);
+            }
         }
         private void FinishAction()
         {
-                
-            _lightBallExplosionParticle.SetActive(true);
-            EventManager.Instance.Broadcast<MatchData>(GameEvents.AddMatchToHandle,matchData);
-            _board.GetCell(_position).SetIsLocked( false);
-            foreach (var match in matchData.Matches)
+            if (!isExplosionInitiated)
             {
-                if(match.IsMatch)
-                    _board.GetCell(match.Pos).SetIsLocked(false);
+                if(_audioSource!=null) 
+                    _audioSource.Stop();
+                AudioManager.Instance.PlaySFX(SFXClips.MatchSound);
+                _chargingParticleEffects.SetActive(false);
+                _lightBallSprite.SetActive(false);
+                _chargingParticleEffects.SetActive(false);
+                HighlightLightBall(false);
+                _lightBallExplosionParticles.SetActive(true);
+                _lightBallExplosionParticles.GetComponent<ParticleSystem>().Play(true);
+                isExplosionInitiated = true;
             }
-            ObjectPool.Instance.ReturnBoosterParticleEffect(_itemParticleEffect, _lightBallID);
+            if (_shouldSpawnBooster&&!_isBoosterSpawned)
+            {
+                IBoardItem boardItem = ObjectPool.Instance.GetBoosterItem(_boosterID, LevelGrid.Instance.GetCellCenterWorld(_position), _board);
+                boardItem.Transform.parent = _board._boardInstance.transform;
+                if (_board.GetCell(_position).HasItem)
+                {
+                    ObjectPool.Instance.ReturnItem(_board.GetItem(_position), _board.GetItem(_position).ItemID);
+                }
+                _board.GetCell(_position).SetItem(boardItem);
+                ItemsToExplode.Add(_position);
+                _isBoosterSpawned = true;
+            }
+            if (_finishCounter < ExplosionTime)
+            {
+                _finishCounter += Time.fixedDeltaTime;
+                return;
+            }
+            UnlockBoardAndExplodeItems();
+            ReturnParticleEffect();
+            _lightBallParticleEffectInstance= null;
+            _lightBallExplosionParticles = null;
+            _chargingParticleEffects = null;
+            _lightBallSprite = null;
+            _board.GetCell(_position).SetIsLocked(false);
+            IsFinished = true;
+            _noTargetCounter = 0;
+            if (!_shouldSpawnBooster) LevelManager.Instance.ItemsGettingMatchedByLightBall.Remove(_matchingItemID);
+            
+        }
+
+        private void UnlockBoardAndExplodeItems()
+        {
+            Debug.Log("Unlocking Board and Exploding Items"+ItemsToExplode.Count);
+            foreach (var itemPosition in ItemsToExplode)
+            {
+                if (!_board.GetCell(itemPosition).HasItem) continue;
+                _board.GetItem(itemPosition).IsActive=true;
+                _board.GetItem(itemPosition).OnExplode();
+                _board.GetCell(itemPosition).SetIsLocked(false);
+                Debug.Log("Exploding"+itemPosition);
+                if (!_shouldSpawnBooster)
+                { 
+                    ExplodeAllDirections(itemPosition);
+                }
+
+            }
+        }
+        private void ReturnParticleEffect()
+        {
+            _lightBallExplosionParticles.SetActive(false);
+            _targetedRays.ForEach(ray => ray.ResetRay());
+            _lightBallParticleEffectInstance.SetActive(false);
+            ObjectPool.Instance.ReturnBoosterParticleEffect(_lightBallParticleEffectInstance,_lightBallID);
             EventManager.Instance.Broadcast(GameEvents.OnBoardUnlock);
         }
-       
-        private void AnimateRay(IItem itemToSendRay)
+        private void TryGetItemPositionForRay(int idToMatch)
         {
-            Vector3 endPosition=itemToSendRay.Transform.position;
-            Vector3 currentEndPos = _lightBallPosition;
-            Vector3 currentStartPos = _lightBallPosition;
-            _lineRenderer.SetPosition(0, currentStartPos);
-            float lerpTime = 0.0f;
-            while (Vector3.Distance(endPosition, currentEndPos) > 0.1f)
+            for (var x = 0; x < _board.Width; x++)
             {
-                lerpTime += Time.fixedDeltaTime* _lerpSpeed;
-                currentEndPos = Vector3.Lerp(currentEndPos, endPosition, lerpTime);
-                _lineRenderer.SetPosition(1, currentEndPos);
-                return;
-            }
-            itemToSendRay.Highlight(1);
-            lerpTime = 0.0f;
-            while (Vector3.Distance(endPosition, currentStartPos) > 0.1f)
-            {
-                lerpTime += Time.fixedDeltaTime;
-                currentStartPos= Vector3.Lerp(currentStartPos, endPosition, lerpTime);
-                _lineRenderer.SetPosition(0, currentStartPos);
-                return;
-            }
-            
-            
-        }
-
-        private bool TryGetItemToMatch(Board board,int idToMatch)
-        {
-
-            for (int x = 0; x < board.Width; x++)
-            {
-                for (int y = 0; y < board.Height; y++)
+                for (var y = 0; y < _board.Height; y++)
                 {
-                    if (board.GetCell(x,y).HasItem && board.GetItem(x,y).ItemID ==idToMatch && !board.GetItem(x,y).IsExploding&&!board.GetItem(x,y).IsMoving&&!board.GetCell(x,y).IsLocked)
+                    var cell = _board.GetCell(x, y);
+                    if (!cell.HasItem)
+                        continue;
+                    if (cell.BoardItem.ItemID == idToMatch && !cell.BoardItem.IsExploding && !cell.BoardItem.IsMoving &&
+                        !cell.IsLocked && !cell.BoardItem.IsMatching&& !ItemsToExplode.Contains(cell.CellPosition))
                     {
-                        Cell cell = board.GetCell(x, y);
-                        cell.SetIsLocked( true);
-                        _itemToSendRay = cell.Item;
-                        return true;
+                        cell.BoardItem.IsActive = false;
+                        ItemsToExplode.Add(cell.CellPosition);
+                        _rayDestinationQueue.Enqueue(cell.CellPosition);
                     }
                 }
             }
 
-            return false;
-        } 
-      
-        
+        }
+        private void ExplodeAllDirections(Vector2Int pos)
+        {
+            foreach (var direction in pos.GetFourDirections())
+                if (_board.IsInBoundaries(direction) && _board.GetCell(direction).HasItem &&
+                    !_board.GetItem(direction).IsExploding && !_board.GetItem(direction).IsMatching &&
+                    _board.GetItem(direction).IsExplodeAbleByNearMatches)
+                    _board.GetItem(direction).OnExplode();
+        }
     }
 }
