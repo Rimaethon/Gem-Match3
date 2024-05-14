d8464e4ba79c446aedca0eae810bb4ae68715148
Yoldaş Ulaş <yutalas@gmail.com>
2024-05-14 18:20:43 +0300

    InputHandler.cs Updated
    Now it handles movement of items without using Dotween since I find out that is very resource intensive thing to do each time user swipes. Instead I lerp the items in a async method. Also action system is updated and I send lightweight events instead of a whole class.

--- Assets/_Scripts/Core/InputHandler.cs
+++ Assets/_Scripts/Core/InputHandler.cs
@@ -4,7 +4,8 @@ using Cysharp.Threading.Tasks;
 using DG.Tweening;
 using Rimaethon.Scripts.Managers;
 using Scripts;
-using UnityEditor;
+using Scripts.BoosterActions;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace _Scripts.Core
@@ -15,39 +16,59 @@ namespace _Scripts.Core
         private Vector2Int _swipeTargetPos;
         private bool _isSwipedThisFrame;
         private  Board _board;
-        private readonly MatchChecker _matchChecker;
+        private  MatchChecker _matchChecker;
         private bool _isSwiping;
-        private MergeHandler _mergeHandler;
+        private bool isDisabled;
 
-        public InputHandler(Board board,MatchChecker matchChecker,MergeHandler mergeHandler)
+        public InputHandler(Board board,MatchChecker matchChecker)
         {
             _board = board;
             _matchChecker = matchChecker;
-            _mergeHandler= mergeHandler;
+            
             EventManager.Instance.AddHandler<Vector2Int, Vector2>(GameEvents.OnSwipe, OnSwipe);
             EventManager.Instance.AddHandler<Vector2>(GameEvents.OnClick, OnClick);
             EventManager.Instance.AddHandler<Vector2>(GameEvents.OnTouch, OnTouch);
         }
-      
+        public void OnDisable()
+        {
+            isDisabled = true;
+            _board = null;
+            _matchChecker = null;
+            _isSwipedThisFrame = false;
+            _isClickedThisFrame = false;
+            if (EventManager.Instance == null)
+            {
+                Debug.Log("Event Manager is null");
+                return;
+            }
+            EventManager.Instance.RemoveHandler<Vector2Int, Vector2>(GameEvents.OnSwipe, OnSwipe);
+            EventManager.Instance.RemoveHandler<Vector2>(GameEvents.OnClick, OnClick);
+            EventManager.Instance.RemoveHandler<Vector2>(GameEvents.OnTouch, OnTouch);
+            
+        }
         public void HandleInputs()
         {
-            if (_isClickedThisFrame)
+            if (isDisabled)
+            {
+                return;
+            }
+            if (_isClickedThisFrame&&!_isSwiping)
             {
                 HandleClick();
             }
+           
             if (!_isSwipedThisFrame||_isSwiping)
             {
                 return;
             }
-
             if (!_board.IsInBoundaries(_swipedItemPos) ||!_board.IsInBoundaries(_swipeTargetPos) ||
-                !_board.GetCell(_swipedItemPos).HasItem || !_board.GetCell(_swipeTargetPos).HasItem)
+                !_board.Cells[_swipedItemPos.x,_swipedItemPos.y].HasItem || !_board.Cells[_swipeTargetPos.x,_swipeTargetPos.y].HasItem)
             {
                 _isSwipedThisFrame = false;
                 return;
             }
-            if (!_board.GetItem(_swipedItemPos).IsSwappable || !_board.GetItem(_swipeTargetPos).IsSwappable||_board.GetItem(_swipeTargetPos).IsMoving||_board.GetItem(_swipedItemPos).IsMoving||_board.GetItem(_swipeTargetPos).IsSwapping||_board.GetItem(_swipedItemPos).IsSwapping||_board.GetItem(_swipeTargetPos).IsExploding||_board.GetItem(_swipedItemPos).IsExploding||_board.GetCell(_swipeTargetPos).IsGettingFilled
-                ||_board.GetCell(_swipedItemPos).IsGettingFilled||_board.GetCell(_swipeTargetPos).IsGettingEmptied||_board.GetCell(_swipedItemPos).IsGettingEmptied||_board.GetCell(_swipeTargetPos).IsLocked||_board.GetCell(_swipedItemPos).IsLocked)
+            if (!_board.GetItem(_swipedItemPos).IsSwappable || !_board.GetItem(_swipeTargetPos).IsSwappable||_board.GetItem(_swipeTargetPos).IsMoving||_board.GetItem(_swipedItemPos).IsMoving||_board.GetItem(_swipeTargetPos).IsSwapping||_board.GetItem(_swipedItemPos).IsSwapping||_board.GetItem(_swipeTargetPos).IsExploding||_board.GetItem(_swipedItemPos).IsExploding||_board.Cells[_swipeTargetPos.x, _swipeTargetPos.y].IsGettingFilled
+                ||_board.Cells[_swipedItemPos.x, _swipedItemPos.y].IsGettingFilled||_board.Cells[_swipeTargetPos.x, _swipeTargetPos.y].IsGettingEmptied||_board.Cells[_swipedItemPos.x, _swipedItemPos.y].IsGettingEmptied||_board.Cells[_swipeTargetPos.x, _swipeTargetPos.y].IsLocked||_board.Cells[_swipedItemPos.x, _swipedItemPos.y].IsLocked)
             {
                 _isSwipedThisFrame = false;
                 return;
@@ -69,22 +90,23 @@ namespace _Scripts.Core
 
             if(item1.IsBooster&&item2.IsBooster)
             {
-                var itemTween = item1.Transform.DOLocalMove(LevelGrid.Instance.GetCellCenterLocalVector2(pos2), 0.2f);
-                await UniTask.WhenAll(itemTween.ToUniTask());
+                await MoveItemAsync(item1.Transform, LevelGrid.Instance.GetCellCenterLocalVector2(pos2), 0.2f);
                 item1.IsActive = false;
                 item2.IsActive = false;
                 item1.IsSwapping = false;
                 item2.IsSwapping = false;
                 _isSwiping = false;
                 _isSwipedThisFrame = false;
-                _mergeHandler.MergeItems(item1,item2);
+                //SMALL BUT DOES A LOT
+                EventManager.Instance.Broadcast(GameEvents.AddMergeActionToHandle, pos1, pos2);
+                item1.OnRemove();
+                item2.OnRemove();
                 return;
             }
-            var item1Tween = item1.Transform.DOLocalMove(LevelGrid.Instance.GetCellCenterLocalVector2(pos2), 0.2f);
-            var item2Tween = item2.Transform.DOLocalMove(LevelGrid.Instance.GetCellCenterLocalVector2(pos1), 0.2f);
-            await UniTask.WhenAll(item1Tween.ToUniTask(), item2Tween.ToUniTask());
+            
+            await UniTask.WhenAll(MoveItemAsync(item1.Transform,LevelGrid.Instance.GetCellCenterLocalVector2(pos2),0.2f),MoveItemAsync(item2.Transform,LevelGrid.Instance.GetCellCenterLocalVector2(pos1),0.2f));
             SwapItems(pos1,pos2);
-            if (item1.IsBooster || item2.IsBooster)
+            if ((item1.IsBooster && _board.Cells[pos2.x,pos2.y].HasItem )|| (item2.IsBooster&& _board.Cells[pos1.x,pos1.y].HasItem))
             {
                 _matchChecker.CheckMatch(!item1.IsBooster ? pos2 : pos1);
                 item1.OnSwap(item1,item2);
@@ -98,10 +120,8 @@ namespace _Scripts.Core
                 bool swap2 = _matchChecker.CheckMatch(pos2);
                 if (!swap1 && !swap2)
                 {
-                    item1Tween = item1.Transform.DOLocalMove(LevelGrid.Instance.GetCellCenterLocalVector2(pos1), 0.2f);
-                    item2Tween = item2.Transform.DOLocalMove(LevelGrid.Instance.GetCellCenterLocalVector2(pos2), 0.2f);
                     AudioManager.Instance.PlaySFX(SFXClips.SwapBackWardSound);
-                    await UniTask.WhenAll(item1Tween.ToUniTask(), item2Tween.ToUniTask());
+                    await UniTask.WhenAll(MoveItemAsync(item1.Transform,LevelGrid.Instance.GetCellCenterLocalVector2(pos1),0.2f),MoveItemAsync(item2.Transform,LevelGrid.Instance.GetCellCenterLocalVector2(pos2),0.2f));
                     SwapItems(pos1,pos2);
                 }
                 else
@@ -116,14 +136,34 @@ namespace _Scripts.Core
             _isSwipedThisFrame = false;
 
         }
+        private async UniTask MoveItemAsync(Transform itemTransform, Vector3 targetPos, float duration)
+        {
+            float elapsedTime = 0;
+
+            Vector3 startingPos = itemTransform.localPosition;
+
+            while (elapsedTime < duration)
+            {
+                itemTransform.localPosition = Vector3.Lerp(startingPos, targetPos, elapsedTime / duration);
+                elapsedTime += Time.fixedDeltaTime;
+                await UniTask.Yield();
+            }
+
+            itemTransform.localPosition = targetPos;
+        }
+        
         private void SwapItems(Vector2Int pos1, Vector2Int pos2)
         {
             IBoardItem temp = _board.GetItem(pos1);
-            _board.GetCell(pos1).SetItem(_board.GetItem(pos2));
-            _board.GetCell(pos2).SetItem(temp);
+            _board.Cells[pos1.x,pos1.y].SetItem(_board.GetItem(pos2));
+            _board.Cells[pos2.x,pos2.y].SetItem(temp);
         }
         private  void OnSwipe(Vector2Int direction, Vector2 clickPos)
         {
+            if (isDisabled)
+            {
+                return;
+            }
             if (_isSwipedThisFrame)
             {
                 return;
@@ -132,8 +172,8 @@ namespace _Scripts.Core
             Vector2Int swipingTo= swipingFrom + direction;
             if(!_board.IsInBoundaries(swipingFrom)||!_board.IsInBoundaries(swipingTo)||_board.GetItem(swipingFrom)==null||_board.GetItem(swipingTo)==null)
                 return;
-            if (!_board.GetItem(swipingFrom).IsSwappable || !_board.GetItem(swipingTo).IsSwappable||_board.GetItem(swipingTo).IsMoving||_board.GetItem(swipingFrom).IsMoving||_board.GetItem(swipingTo).IsSwapping||_board.GetItem(swipingFrom).IsSwapping||_board.GetItem(swipingTo).IsExploding||_board.GetItem(swipingFrom).IsExploding||_board.GetCell(swipingTo).IsGettingFilled
-                ||_board.GetCell(swipingFrom).IsGettingFilled||_board.GetCell(swipingTo).IsGettingEmptied||_board.GetCell(swipingFrom).IsGettingEmptied||_board.GetCell(swipingTo).IsLocked||_board.GetCell(swipingFrom).IsLocked||!_board.GetItem(swipingFrom).IsActive||!_board.GetItem(swipingTo).IsActive)
+            if (!_board.GetItem(swipingFrom).IsSwappable || !_board.GetItem(swipingTo).IsSwappable||_board.GetItem(swipingTo).IsMoving||_board.GetItem(swipingFrom).IsMoving||_board.GetItem(swipingTo).IsSwapping||_board.GetItem(swipingFrom).IsSwapping||_board.GetItem(swipingTo).IsExploding||_board.GetItem(swipingFrom).IsExploding||_board.Cells[swipingTo.x,swipingTo.y].IsGettingFilled
+                ||_board.Cells[swipingFrom.x,swipingFrom.y].IsGettingFilled||_board.Cells[swipingTo.x,swipingTo.y].IsGettingEmptied||_board.Cells[swipingFrom.x,swipingFrom.y].IsGettingEmptied||_board.Cells[swipingTo.x,swipingTo.y].IsLocked||_board.Cells[swipingFrom.x,swipingFrom.y].IsLocked||!_board.GetItem(swipingFrom).IsActive||!_board.GetItem(swipingTo).IsActive)
             {
                 return;
             }
@@ -151,8 +191,8 @@ namespace _Scripts.Core
             }
             _isClickedThisFrame = false;
             if (!_board.IsInBoundaries(_clickPos)) return;
-            if(!_board.GetCell(_clickPos).HasItem) return;
-            if (_board.GetCell(_clickPos).HasItem)
+            if(!_board.Cells[_clickPos.x,_clickPos.y].HasItem) return;
+            if (_board.Cells[_clickPos.x,_clickPos.y].HasItem)
             {
                 _board.GetItem(_clickPos).OnClick(_board,_clickPos);
                 if (_board.GetItem(_clickPos).IsBooster)
@@ -161,7 +201,7 @@ namespace _Scripts.Core
                 }
                 
             }
-            if(_board.GetCell(_clickPos).HasItem)
+            if(_board.Cells[_clickPos.x,_clickPos.y].HasItem)
                 _board.GetItem(_clickPos).OnTouch();;
         }
         private void OnClick(Vector2 clickPosition)
@@ -172,13 +212,18 @@ namespace _Scripts.Core
             }
             
             _clickPos= LevelGrid.Instance.WorldToCellVector2Int(clickPosition);
+      
             _isClickedThisFrame = true;
                         
         }
         private void OnTouch(Vector2 touchPos)
         {
+            if (isDisabled)
+            {
+                return;
+            }
             Vector2Int firstCellPos = LevelGrid.Instance.WorldToCellVector2Int(touchPos);
-            if (_board.IsInBoundaries(firstCellPos)&&_board.GetCell(firstCellPos).HasItem)
+            if (_board.IsInBoundaries(firstCellPos)&&_board.Cells[firstCellPos.x,firstCellPos.y].HasItem)
             {
                 _board.GetItem(firstCellPos).OnTouch();
             }
