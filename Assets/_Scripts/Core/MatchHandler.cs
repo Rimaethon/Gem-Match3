998de0924ed1d79d8b1327450fb20c3794a63589
Yoldaş Ulaş <yutalas@gmail.com>
2024-05-15 12:19:14 +0300

    Lerp Logic and Handle Matches method Updated and  OnDisable Method added to MatchHandler.cs
    Lerp Logic was also using Dotween and I find it better to use MoveTowards Method instead. Also ExplodeAllDirections Method added in order to explode near obstacles and generator items

--- Assets/_Scripts/Core/MatchHandler.cs
+++ Assets/_Scripts/Core/MatchHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using _Scripts.Core;
 using _Scripts.Utility;
 using Cysharp.Threading.Tasks;
@@ -13,111 +14,145 @@ namespace _Scripts.Managers.Matching
     {
         private Board _board;
         private Sequence _sequence;
-        private bool[] _dirtyColumns;
         private readonly HashSet<MatchData> _matchesToHandle= new HashSet<MatchData>(); 
-        private readonly HashSet<MatchData> _matchesToHandleThisFrame= new HashSet<MatchData>(); 
-        public MatchHandler(Board board, bool[] dirtyColumns)
+        private List<MatchData> _matchesToHandleThisFrame= new List<MatchData>();
+        private List<Vector2Int> _directions = new List<Vector2Int>();
+        private bool isDisabled;
+        public MatchHandler(Board board)
         {
             _board = board;
-            _dirtyColumns = dirtyColumns;
             EventManager.Instance.AddHandler<MatchData>(GameEvents.AddMatchToHandle, AddMatchToHandle);
-
-            
+        }
+        
+        public void OnDisable()
+        {
+            _board = null;
+            isDisabled = true;
+            _matchesToHandle.Clear();
+            _matchesToHandleThisFrame.Clear();
+            _directions.Clear();
+            GC.Collect();
+            if (EventManager.Instance == null) return;
+            EventManager.Instance.RemoveHandler<MatchData>(GameEvents.AddMatchToHandle, AddMatchToHandle);
         }
         private void AddMatchToHandle(MatchData matchData)
         {
-            
+            if (isDisabled)
+            {
+                Debug.LogWarning("MatchHandler is disabled");
+                return;
+            }
             _matchesToHandle.Add(matchData);
         }
 
 
-        public void HandleMatches()
+        public bool HandleMatches()
         {
-            _matchesToHandleThisFrame.UnionWith(_matchesToHandle);
-            
+            if (isDisabled)
+            {
+                Debug.LogWarning("MatchHandler is disabled");
+                return false;
+            }
+            _matchesToHandleThisFrame.AddRange(_matchesToHandle);
             foreach (MatchData matchData in _matchesToHandleThisFrame)
             {
                 CheckForMatchType(matchData);
-                _matchesToHandle.Remove(matchData);
             }
             _matchesToHandleThisFrame.Clear();
-            
+            return _matchesToHandle.Count > 0;
         }
         
         private void CheckForMatchType(MatchData data)
         {
-            if(data.MatchType==MatchType.None)
+            if (data.MatchType == MatchType.None)
+            {
+                _matchesToHandle.Remove(data);
                 return;
+            }
             if (data.MatchType == MatchType.Normal)
             {
                 HandleMatch(data.Matches);
-                return;
-            }
-            if (data.MatchType == MatchType.SingleExplosion)
-            {
-               HandleExplosion(data.Matches);
+                _matchesToHandle.Remove(data);
                 return;
             }
             LerpAllItemsToPosition(data);
         }
-
-        public void LerpAllItemsToPosition(MatchData matchData)
+        private void  HandleMatch(List<Vector2Int> matchedItems)
         {
-            Vector2Int mergeVector2IntPos = matchData.Matches[0].Pos;
-            _board.GetCell(mergeVector2IntPos).SetIsLocked(true);
-            matchData.Matches[0].IsMatch = false;
-
+            foreach (var pos in matchedItems)
+            {
+                if(!_board.IsInBoundaries(pos)||!_board.Cells[pos.x,pos.y].HasItem||!_board.GetItem(pos).IsMatching)
+                    continue;
+                ExplodeAllDirections(pos);
+                Cell cell=_board.Cells[pos.x,pos.y];
+                cell.BoardItem.OnMatch();
+            }
+            AudioManager.Instance.PlaySFX(SFXClips.MatchItemExplodeSound);
+            IBoardItem item = _board.GetItem(matchedItems[0]);
+            EventManager.Instance.Broadcast<Vector3,int,int>(GameEvents.OnMainEventGoalMatch,item.Transform.position,item.ItemID,matchedItems.Count);
+        }
+        private void LerpAllItemsToPosition(MatchData matchData)
+        {
+            if (!matchData.IsInitialized)
+            {
+                InitializeLerp(matchData);
+            }
+            bool isAllMerged = true;
+            Vector2Int mergeVector2IntPos = matchData.Matches[0];
             Vector3 mergePos = LevelGrid.Instance.GetCellCenterLocalVector2(mergeVector2IntPos);
-            foreach (Match match in matchData.Matches)
+            foreach (Vector2Int pos in matchData.Matches)
             {
-                if (!IsInBounds(match.Pos) || !_board.GetCell(match.Pos).HasItem || !match.IsMatch)
+                if (!_board.IsInBoundaries(pos) || !_board.Cells[pos.x,pos.y].HasItem)
                     continue;
-                _board.GetCell(match.Pos).SetIsLocked(true);
-                float duration = Vector2Int.Distance(mergeVector2IntPos, match.Pos) * 0.1f + 0.05f;
-                LerpPosition(match.Pos, mergePos, duration);
+                Cell cell = _board.Cells[pos.x,pos.y];
+                cell.BoardItem.Transform.localPosition = Vector3.MoveTowards(cell.BoardItem.Transform.localPosition, mergePos, 0.09f);
+                if (Vector3.Distance(cell.BoardItem.Transform.localPosition, mergePos) > 0.05f)
+                {
+                    isAllMerged = false;
+                }
             }
-            _board.GetItem(mergeVector2IntPos).OnRemove();
+            if(!isAllMerged)
+                return;
+            foreach (Vector2Int match in matchData.Matches)
+            {
+                if (!_board.IsInBoundaries(match) || !_board.Cells[match.x,match.y].HasItem)
+                    continue;
+                ExplodeAllDirections(match);
+                Cell cell = _board.Cells[match.x,match.y];
+                cell.BoardItem.OnRemove();
+            }
+
+            AudioManager.Instance.PlaySFX(SFXClips.MatchSound);
+            IBoardItem item = _board.GetItem(mergeVector2IntPos);
+            EventManager.Instance.Broadcast<Vector3,int,int>(GameEvents.OnMainEventGoalMatch,item.Transform.position,item.ItemID,matchData.Matches.Count);
             EventManager.Instance.Broadcast(GameEvents.AddItemToAddToBoard, mergeVector2IntPos, (int)matchData.MatchType);
-            _board.GetCell(mergeVector2IntPos).SetIsLocked(false);
+            _matchesToHandle.Remove(matchData);
         }
 
-        private void LerpPosition(Vector2Int pos, Vector3 targetPosition, float duration)
+        private void InitializeLerp(MatchData matchData)
         {
-            IItem item = _board.GetItem(pos);
-            item.Transform.DOLocalMove(targetPosition, duration).SetEase(Ease.Linear);
-            DOVirtual.DelayedCall(duration, () =>
+            foreach (Vector2Int match in matchData.Matches)
             {
-                _board.GetCell(pos).SetIsLocked(false);
-               item.OnRemove();
-            });
-        }
-
-        private void  HandleMatch(Match[] matchedItems)
-        {
-            foreach (var match in matchedItems)
-            {
-                if(!IsInBounds(match.Pos)||!_board.GetCell(match.Pos).HasItem||!match.IsMatch||!_board.GetItem(match.Pos).IsMatching)
+                if (!_board.IsInBoundaries(match) || !_board.Cells[match.x,match.y].HasItem)
                     continue;
-                Cell cell=_board.GetCell(match.Pos);
-                cell.Item.OnMatch();
+                Cell cell = _board.Cells[match.x,match.y];
+                cell.BoardItem.IsMatching = true;
+                cell.BoardItem.IsActive= false;
+            }
+            matchData.IsInitialized = true;
+        }
+            
+        private void ExplodeAllDirections(Vector2Int pos)
+        {
+           _directions = pos.GetFourDirections();
+            for (int i = 0; i < 4; i++)
+            {
+                if (_board.IsInBoundaries(_directions[i]) && _board.Cells[_directions[i].x,_directions[i].y].HasItem &&
+                    !_board.GetItem(_directions[i]).IsExploding && !_board.GetItem(_directions[i]).IsMatching &&
+                    _board.GetItem(_directions[i]).IsExplodeAbleByNearMatches)
+                    _board.GetItem(_directions[i]).OnExplode();
             }
         }
-
-        private void HandleExplosion(Match[] explodingItems)
-        {
-            foreach (var match in explodingItems)
-            {
-                if(!IsInBounds(match.Pos)||!_board.GetCell(match.Pos).HasItem||!match.IsMatch)
-                    continue;
-                Cell cell=_board.GetCell(match.Pos);
-                cell.Item.OnExplode();
-            }
-        }
-        private bool IsInBounds(Vector2Int pos)
-        {
-            return pos.x >= 0 && pos.x < _board.Width && pos.y >= 0 && pos.y < _board.Height;
-        }
-    
         
     }
 }
