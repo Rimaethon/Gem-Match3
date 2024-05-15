6e14fd0315a0c3a215198675ceaa0214bae2ab56
Yoldaş Ulaş <yutalas@gmail.com>
2024-05-15 16:32:13 +0300

    UserData.cs Updated And BoosterData class created
    It was holding too little for the scale of this project. Settings data added booster data restructured since it wasn't able to manage unlimited boosters. Also Heart data and its timers are added.

--- Assets/_Scripts/Data Classes/UserData.cs
+++ Assets/_Scripts/Data Classes/UserData.cs
@@ -7,9 +7,31 @@ namespace Data
     public class UserData
     {
         public int currentLevel=1;
-        public int coinCount=1000;
-        public int heartCount=5;
-        public int starCount=0;
-        public Dictionary<int, int> BoosterCounts;
+        public int coinAmount=3000;
+        public int heartAmount=5;
+        public int starAmount=0;
+        public bool hasUnlimitedHearts;
+        public long unlimitedHeartStartTime;
+        public long unlimitedHeartDuration;
+        public int maxHeartAmount=5;
+        //This value will store when first of 5(max) hearts is used 
+        public long firstHeartNotBeingFullUnixTime;
+        public int heartRefillTimeInSeconds=1200;
+        public Dictionary<int, BoosterData> BoosterAmounts;
+        public Dictionary<int, int> PowerUpAmounts;
+        public bool isMusicOn=true;
+        public bool isSfxOn=true;
+        public bool isNotificationOn=true;
+        public bool isHintOn=true;
     }
+    [Serializable]
+    public class BoosterData
+    {
+        public int boosterAmount;
+        public bool isUnlimited;
+        public long unlimitedStartTime;
+        public long unlimitedDuration;
+
+    }
+
 }
