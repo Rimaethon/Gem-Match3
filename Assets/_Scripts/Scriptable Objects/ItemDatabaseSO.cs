d032ccbfdd9f6a0b8b3f0a5e35b3f1225effd3f2
Yoldaş Ulaş <yutalas@gmail.com>
2024-05-18 17:27:15 +0300

    Background Dictionary and Spesific Booster Effects are added to ItemDatabaseSO.asset
    I'm not happy with putting some effects outside of my generic dictionaries but I couldn't find a better solution which would not need me spend days on it.

--- Assets/_Scripts/Scriptable Objects/ItemDatabaseSO.cs
+++ Assets/_Scripts/Scriptable Objects/ItemDatabaseSO.cs
@@ -9,16 +9,25 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ItemDatabase", menuName = "Create ItemDatabase")]
 public class ItemDatabaseSO : SerializedScriptableObject
 {
+    //Some Effects are defined explicitly because It seemed unnecessary to change whole ItemData architecture for these since every item has one particle effect most of the time. 
     [DictionaryDrawerSettings(KeyLabel = "Item ID", ValueLabel = "Item Data")]
-    public GameObject boosterCreationEffect;
-    public GameObject matchEffect;
-    public Dictionary<int, ItemData> NormalItems = new Dictionary<int, ItemData>();
-    public Dictionary<int, ItemData> Boosters = new Dictionary<int, ItemData>();
-    public List<Tuple<int,int,IItemMerge>> BoosterMergeAction = new List<Tuple<int, int,IItemMerge>>();
-    public Dictionary<int, BoardSpriteSaveData> Boards = new Dictionary<int, BoardSpriteSaveData>();
+    public readonly GameObject boosterCreationEffect;
+    public readonly GameObject missileHitEffect;
+    public readonly GameObject missileExplosionEffect;
+    public readonly GameObject LightBallLightBallExplosionEffect;
+    public readonly GameObject TntRocketMergeParticleEffect;
+    public readonly GameObject TntTntExplosionEffect;
+    public readonly GameObject starParticleEffect;
+    public readonly GameObject mainEventUIEffect;
+    public readonly int coinID;
+    public readonly  Dictionary<int, ItemData> NormalItems = new Dictionary<int, ItemData>();
+    public readonly Dictionary<int, ItemData> Boosters = new Dictionary<int, ItemData>();
+    public readonly List<Tuple<int,int,IItemMergeAction>> BoosterMergeAction = new List<Tuple<int, int,IItemMergeAction>>();
+    public readonly Dictionary<int, BoardSpriteSaveData> Boards = new Dictionary<int, BoardSpriteSaveData>();
+    public readonly Dictionary<int,Sprite> Backgrounds = new Dictionary<int, Sprite>();
     
     
-    [Button("Set Item ID's")]
+    [Button("Set Item ID's to Prefabs")]
     public void Initialize()
     {
         foreach (KeyValuePair<int,ItemData> item in NormalItems)
@@ -31,7 +40,7 @@ public class ItemDatabaseSO : SerializedScriptableObject
         }
         foreach (KeyValuePair<int,ItemData> item in Boosters)
         {
-            if(item.Value.ItemPrefab!=null)
+            if(item.Value.ItemPrefab!=null&&item.Value.ItemPrefab.GetComponent<IItem>()!=null)
                 item.Value.ItemPrefab.GetComponent<IItem>().ItemID = item.Key;
             if (item.Value.ItemParticleEffect != null)
                 item.Value.ItemParticleEffect.GetComponent<BoosterParticleEffect>().ItemID = item.Key;
@@ -49,7 +58,17 @@ public class ItemDatabaseSO : SerializedScriptableObject
     }
     public IItemAction GetNormalItemAction(int id)
     {
-        return NormalItems[id].ItemAction;
+        if (NormalItems.ContainsKey(id))
+        {
+            return NormalItems[id].ItemAction;
+            
+        }
+        if(Boosters.ContainsKey(id))
+        {
+            return Boosters[id].ItemAction;
+        }
+        return null;
+     
     }
     public IItemAction GetBoosterItemAction(int id)
     {
@@ -63,18 +82,22 @@ public class ItemDatabaseSO : SerializedScriptableObject
     {
         return Boosters[id].ItemParticleEffect; 
     }
+    public Sprite GetItemSprite(int id)
+    {
+        return NormalItems[id].ItemSprite;
+    }
+    public Sprite GetBoosterSprite(int id)
+    {
+        return Boosters[id].ItemSprite;
+    }
     public GameObject GetBoosterCreationEffect()
     {
         return boosterCreationEffect;
     }
-    public GameObject GetMatchEffect()
-    {
-        return matchEffect;
-    }
+
     public BoardSpriteSaveData GetBoardSpriteData(int id)
     {
         return Boards[id];
     }
     
-
 }
