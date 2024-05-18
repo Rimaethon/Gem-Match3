8565958c371ea92de9774d9f6f2f2e12a412c2d2
Yoldaş Ulaş <yutalas@gmail.com>
2024-05-18 11:36:21 +0300

    Cell.cs Updated
    Getters are added for each layer of items and cell locking logic changed in order to handle multiple lock and unlocks.

--- Assets/_Scripts/Items/Cell.cs
+++ Assets/_Scripts/Items/Cell.cs
@@ -1,58 +1,79 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Scripts
 {
     public class Cell
     {
-        public Cell(Vector2Int cellPosition,IItem item=null,IItem underLayItem=null,IItem overLayItem=null,bool isNotInBoard=false)
+        public Cell(Vector2Int cellPosition,IBoardItem boardItem=null,IBoardItem underLayBoardItem=null,IBoardItem overLayBoardItem=null,bool isNotInBoard=false)
         {
             CellPosition = cellPosition;
-            SetItem(item);
-            UnderLayItem = underLayItem;
-            OverLayItem = overLayItem;
+            SetItem(boardItem);
+            _underLayBoardItem = underLayBoardItem;
+            _overLayBoardItem= overLayBoardItem;
             IsNotInBoard = isNotInBoard;
         }
         public bool IsNotInBoard;
-        public bool HasItem => Item != null;
-        public bool HasUnderLayItem => UnderLayItem != null;
-        public bool HasOverLayItem => OverLayItem != null;
+        public bool HasItem => BoardItem != null;
+        public bool HasUnderLayItem => UnderLayBoardItem != null;
+        public bool HasOverLayItem => OverLayBoardItem != null;
         public Vector2Int CellPosition;
         public bool IsGettingFilled => _isGettingFilled;
         public bool IsGettingEmptied => _isGettingEmptied;
         public bool IsLocked => _isLocked;
-        public IItem Item=>_item;
-        private IItem _item;
-        public IItem UnderLayItem;
-        public IItem OverLayItem;
+        public IBoardItem BoardItem=>_boardItem;
+        private IBoardItem _boardItem;
+        public IBoardItem UnderLayBoardItem=>_underLayBoardItem;
+        private IBoardItem _underLayBoardItem;
+        public IBoardItem OverLayBoardItem=>_overLayBoardItem;
+        private IBoardItem _overLayBoardItem;
         private bool _isLocked;
         private bool _isGettingFilled ;
         private bool _isGettingEmptied;
-        
-        public void SetItem(IItem item)
+        private int _lockCount=0;
+        public void SetItem(IBoardItem boardItem)
         {
-            _item = item;
-            if (Item != null)
+            _boardItem = boardItem;
+            if (BoardItem != null)
             {
-                Item.Position = CellPosition;
+                BoardItem.Position = CellPosition;
+            } 
+        }
+        public void SetUnderLayItem(IBoardItem boardItem)
+        {
+            _underLayBoardItem = boardItem;
+            if (UnderLayBoardItem != null)
+            {
+                UnderLayBoardItem.Position = CellPosition;
             }
-          Debug.Log("Item set to "+item+" for cell "+CellPosition.x+" "+CellPosition.y);
+        }
+        public void SetOverLayItem(IBoardItem boardItem)
+        {
+            _overLayBoardItem = boardItem;
+            if (OverLayBoardItem != null)
+            {
+                OverLayBoardItem.Position = CellPosition;
+            }
         }
         
         public void SetIsGettingFilled(bool value)
         {
             _isGettingFilled = value;
-           Debug.Log("Is getting filled set to "+value+" for cell "+CellPosition.x+" "+CellPosition.y);
         }
         public void SetIsGettingEmptied(bool value)
         {
             _isGettingEmptied = value;
-       Debug.Log("Is getting Emptied set to "+value+" for cell "+CellPosition.x+" "+CellPosition.y);
         }
         public void SetIsLocked(bool value)
         {
-            _isLocked = value;
-       Debug.Log("Is getting Locked set to "+value+" for cell "+CellPosition.x+" "+CellPosition.y);
+            _lockCount=value?_lockCount+1:_lockCount-1;
+            if (_lockCount <= 0)
+            {
+                _isLocked = false;
+                
+            }else
+            {
+                _isLocked = true;
+            }
         }
     }
 }
