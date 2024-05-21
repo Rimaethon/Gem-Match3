3602715d4db02d969d8d72bec1d9abd80dbf3736
Yoldaş Ulaş <yutalas@gmail.com>
2024-05-21 17:30:43 +0300

    Level2Board.png added and CoinResourceBar.cs is created
    Now I have a button that opens IAP panel so that players can buy things for free.Also Level2Board added but my logic is currently not accommodating cells that are used as pass through only since I'm not checking if there is enough space below before moving any item.

---
+++ Assets/_Scripts/UI/ResourceBars/CoinResourceBar.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Data;
+using DG.Tweening;
+using Rimaethon.Runtime.UI;
+using Rimaethon.Scripts.Managers;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace _Scripts.UI.Panels
+{
+    public class CoinResourceBar:UIButton
+    {
+        [SerializeField] private TMP_Text _coinText;
+        [SerializeField] private GameObject coinPanel;
+        [SerializeField] private bool _isClickAble;
+        
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            EventManager.Instance.AddHandler(GameEvents.OnCoinAmountChanged, UpdateCoinAmount);
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+            if (EventManager.Instance==null)
+                return;
+            EventManager.Instance.RemoveHandler(GameEvents.OnCoinAmountChanged, UpdateCoinAmount);
+        }
+        private void Start()
+        {
+            UpdateCoinAmount();
+        }
+
+        private void UpdateCoinAmount()
+        {
+            _coinText.text=SaveManager.Instance.GetCoinAmount().ToString();
+
+        }
+        protected override void DoOnClick()
+        {
+            Debug.Log("Clicked");
+            if (!_isClickAble&&coinPanel!=null)
+            {
+                return;
+            }
+            base.DoOnClick();
+            coinPanel.SetActive(true);
+        }
+
+        protected override void DoOnPointerDown()
+        {
+            if (!_isClickAble)
+            {
+                return;
+            }
+
+            base.DoOnPointerDown();
+        }
+
+        protected override void DoOnPointerUp()
+        {
+            if (!_isClickAble)
+            {
+                return;
+            }
+            base.DoOnPointerUp();
+        }
+    }
+}
