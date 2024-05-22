eed1a42be172b17d9f3c2bf5bc8d85f73898e7ee
Yoldaş Ulaş <yutalas@gmail.com>
2024-05-22 10:10:42 +0300

    MatchItemAction.cs is updated and DownWardItemAction.cs is created
     Now MatchItemAction class has a initialize method instead of a constructor and it resets itself since it needs to be reusable. Also it uses value2 as a boolean to decide whether it will play match animation or just explode.

---
+++ Assets/_Scripts/Items/ItemActions/ItemActions/DownWardItemAction.cs
@@ -0,0 +1,49 @@
+﻿using _Scripts.Utility;
+using DG.Tweening;
+using Rimaethon.Scripts.Managers;
+using UnityEngine;
+
+namespace Scripts.DownWardItems
+{
+    //TODO: Implement DownWardItemAction 
+    public class DownWardItemAction: IItemAction
+    {
+        private Vector2Int _pos;
+        private Vector3 _goalUIPos;
+        private readonly Vector3 _moveDownOffset = new(0, 0.1f, 0);
+        private GameObject _particleEffect;
+        public bool IsFinished => _isFinished;
+        public int ItemID { get; set; }
+        public Board Board { get; set; }
+        private bool _isFinished;
+
+        public void InitializeAction(Board board, Vector2Int pos, int value1, int value2)
+        {
+            _pos = pos;
+            Board = board;
+            _goalUIPos = InGameUIManager.Instance.GetGoalPosition(board.GetItem(pos).ItemID);
+            _particleEffect =
+                ObjectPool.Instance.GetItemParticleEffect(Board.GetItem(_pos).ItemID,
+                    Board.GetItem(_pos).Transform.position);
+            _particleEffect.transform.DOMove(_particleEffect.transform.position - _moveDownOffset, 0.3f)
+                .SetUpdate(UpdateType.Fixed);
+            _particleEffect.transform.DOScale(Vector3.one, 0.3f).SetUpdate(UpdateType.Fixed).OnComplete(() =>
+            {
+                _particleEffect.transform.DOMove(_goalUIPos, 0.7f).SetUpdate(UpdateType.Fixed).OnComplete(() =>
+                {
+                    _particleEffect.transform.DOScale(Vector3.zero, 0.15f).SetUpdate(UpdateType.Fixed)
+                        .OnComplete(() =>
+                        {
+                            EventManager.Instance.Broadcast(GameEvents.OnItemExplosion, _pos, Board.GetItem(_pos).ItemID);
+                            _isFinished = true;
+                            _particleEffect = null;
+                        });
+                });
+            });
+        }
+
+        public void Execute()
+        {
+        }
+    }
+}
---
+++ Assets/_Scripts/Items/ItemActions/ItemActions/MatchItemAction.cs
@@ -0,0 +1,84 @@
+﻿using _Scripts.Utility;
+using UnityEngine;
+
+namespace Scripts.BoosterActions
+{
+    public enum MatchState
+    {
+        ScaleDown,
+        Explode
+    }
+
+    public class MatchItemAction : IItemAction
+    {
+        private IBoardItem _boardItem;
+        private Vector2Int _pos;
+        private float _explodeTime=0.15f;
+        private float _scaleDownTime=0.15f;
+        private float _counter;
+        private bool _isExplodeInitiated;
+        private MatchState _state=MatchState.Explode;
+        public bool IsFinished { get; set; }
+        public int ItemID { get; set; }
+        public Board Board { get; set; }
+
+        public void InitializeAction(Board board, Vector2Int pos, int value1, int value2)
+        {
+            _state = value2==0 ? MatchState.ScaleDown : MatchState.Explode;
+            _boardItem = board.GetItem(pos);
+            Board = board;
+            _pos = pos;
+            IsFinished = false;
+            _counter = 0;
+            _isExplodeInitiated = false;
+        }
+
+        public void Execute()
+        {
+            switch (_state)
+            {
+                case MatchState.ScaleDown:
+                    ScaleDown();
+                    break;
+                case MatchState.Explode:
+                    Explode();
+                    break;
+            }
+        }
+
+
+        private void ScaleDown()
+        {
+            if (_counter < _scaleDownTime)
+            {
+                _counter += Time.deltaTime;
+                _boardItem.Transform.localScale = Vector3.Lerp(Vector3.one, Vector3.zero, _counter / _scaleDownTime);
+                return;
+            }
+
+            _counter = 0;
+            _state = MatchState.Explode;
+        }
+
+        private void Explode()
+        {
+            if (!_isExplodeInitiated)
+            {
+               ObjectPool.Instance.GetItemParticleEffect(_boardItem.ItemID, _boardItem.Transform.position);
+                _isExplodeInitiated = true;
+                Board.Cells[_pos.x,_pos.y].SetIsLocked(true);
+                _boardItem.OnRemove();
+            }
+
+            if (_counter < _explodeTime)
+            {
+                _counter += Time.deltaTime;
+                return;
+            }
+            Board.Cells[_pos.x,_pos.y].SetIsLocked(false);
+            IsFinished = true;
+        }
+
+      
+    }
+}
