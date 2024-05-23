d456dcbade9dafe51b7123b1f1fa09c799b43300
Yoldaş Ulaş <yutalas@gmail.com>
2024-05-23 14:25:16 +0300

    HeartResourceBar.cs and StarResourceBar.cs are created
    They are just updating the amounts when they get an invoke to their events. Maybe in the future they can have a simple panel for saying "Hey you can get starts by winning levels and you can gain hearts with requesting to your team"

---
+++ Assets/_Scripts/UI/ResourceBars/HeartResourceBar.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using _Scripts.Core.Interfaces;
+using Data;
+using DG.Tweening;
+using Rimaethon.Runtime.UI;
+using Rimaethon.Scripts.Managers;
+using TMPro;
+using UnityEngine;
+
+namespace _Scripts.UI.Panels
+{
+    public class HeartResourceBar:UIButton,ITimeDependent
+    {
+        [SerializeField] private TMP_Text _heartText;
+        [SerializeField] TMP_Text _heartRefillTimeText;
+        [SerializeField] private GameObject heartPanel;
+        private bool _isClickAble;
+
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            EventManager.Instance.AddHandler(GameEvents.OnHeartAmountChanged, UpdateHeartVisual);
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+            if (EventManager.Instance==null)
+                return;
+            EventManager.Instance.RemoveHandler(GameEvents.OnHeartAmountChanged, UpdateHeartVisual);
+        }
+
+        
+        public void OnTimeUpdate(long currentTime)
+        {
+            if (SaveManager.Instance.HasUnlimitedHearts())
+            {
+                _heartRefillTimeText.text = SaveManager.Instance.GetUnlimitedHeartTime();
+                return;
+            }
+            UpdateHeartVisual();
+        }
+        private void Start()
+        {
+            UpdateHeartVisual();  
+        }
+
+        private void UpdateHeartVisual()
+        {
+            if(SaveManager.Instance.HasUnlimitedHearts())
+            {
+                _heartText.text="∞";
+            }
+            else
+            {
+                int heartCount=SaveManager.Instance.GetHeartAmount();
+                if (heartCount == SaveManager.Instance.GetMaxHeartAmount())
+                {
+                    _heartRefillTimeText.text = "Full";
+                }
+                else
+                {
+                    heartCount=(int)Math.Clamp((DateTimeOffset.UtcNow.ToUnixTimeSeconds()-SaveManager.Instance.GetFirstHeartNotBeingFullUnixTime())/SaveManager.Instance.GetHeartRefillTimeInSeconds(),heartCount,SaveManager.Instance.GetMaxHeartAmount());
+                }
+                _heartText.text=heartCount.ToString();
+            }
+        }
+        protected override void DoOnClick()
+        {
+            if (!_isClickAble)
+            {
+                return;
+            }
+            base.DoOnClick();
+
+            heartPanel.SetActive(true);
+        }
+
+        protected override void DoOnPointerDown()
+        {
+            if (!_isClickAble)
+            {
+                return;
+            }
+
+            base.DoOnPointerDown();
+        }
+
+        protected override void DoOnPointerUp()
+        {
+            if (!_isClickAble)
+            {
+                return;
+            }
+            base.DoOnPointerUp();
+        }
+
+    }
+}
---
+++ Assets/_Scripts/UI/ResourceBars/StarResourceBar.cs
@@ -0,0 +1,69 @@
+﻿using Data;
+using DG.Tweening;
+using Rimaethon.Runtime.UI;
+using Rimaethon.Scripts.Managers;
+using TMPro;
+using UnityEngine;
+
+namespace _Scripts.UI.Panels
+{
+    public class StarResourceBar : UIButton
+    {
+        [SerializeField] private TMP_Text _starAmountText;
+        [SerializeField] private GameObject starPanel;
+        [SerializeField] private bool _isClickAble;
+        public RectTransform starIconPosition;
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            EventManager.Instance.AddHandler(GameEvents.OnStarAmountChanged, UpdateStarAmount);
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+            if (EventManager.Instance==null)
+                return;
+            EventManager.Instance.RemoveHandler(GameEvents.OnStarAmountChanged, UpdateStarAmount);
+        }
+
+        private void Start()
+        {
+            UpdateStarAmount();
+        }
+        private void UpdateStarAmount()
+        {
+            _starAmountText.text = SaveManager.Instance.GetStarAmount().ToString();
+        }
+        protected override void DoOnClick()
+        {
+            Debug.Log("Clicked");
+            if (!_isClickAble)
+            {
+                return;
+            }
+            base.DoOnClick();
+            starPanel.SetActive(true);
+        }
+
+        protected override void DoOnPointerDown()
+        {
+            if (!_isClickAble)
+            {
+                return;
+            }
+
+            base.DoOnPointerDown();
+        }
+
+        protected override void DoOnPointerUp()
+        {
+            if (!_isClickAble)
+            {
+                return;
+            }
+            base.DoOnPointerUp();
+        }
+    }
+}
