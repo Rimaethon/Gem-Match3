cf82b08950f8ebecd9180822debc093636a3d1c8
Yoldaş Ulaş <yutalas@gmail.com>
2024-05-26 15:43:28 +0300

    UnderlayBoardItem.cs is updated and UnderLayItemAction.cs is created
    Normally Poor UnderlayBoardItem didn't have any action since it was just getting removed with just calling a particle effect. But since my logic doesn't approve actionless Items(It has control issues don't judge it) I created a action for it.

---
+++ Assets/_Scripts/Items/ItemActions/ItemActions/UnderLayItemAction.cs
@@ -0,0 +1,26 @@
+﻿using Scripts;
+using UnityEngine;
+
+namespace _Scripts.Items.ItemActions
+{
+    public class UnderLayItemAction: IItemAction
+    {
+        public bool IsFinished => _isFinished;
+        private bool _isFinished;
+        public int ItemID { get; set; }
+        public Board Board { get; set; }
+
+        public void InitializeAction(Board board, Vector2Int pos, int value1, int value2)
+        {
+            ObjectPool.Instance.GetItemParticleEffect(ItemID, LevelGrid.Instance.GetCellCenterWorld(pos));
+            _isFinished = false;
+        }
+
+        public void Execute()
+        {
+            _isFinished = true;
+        }
+
+    
+    }
+}
---
+++ Assets/_Scripts/Items/Obstacle Items/UnderlayBoardItem.cs
@@ -0,0 +1,34 @@
+﻿using Cysharp.Threading.Tasks;
+using Rimaethon.Scripts.Managers;
+using UnityEngine;
+
+namespace Scripts
+{
+    public class UnderlayBoardItem:BoardItemBase
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+            isFallAble = false;
+            _isMatchable = false;
+            _isSwappable = false;
+        }
+        public override void OnExplode()
+        {
+            if(IsExploding)
+                return;
+            _isExploding= true;
+            EventManager.Instance.Broadcast(GameEvents.AddActionToHandle,_position,_itemID,0);
+            OnRemove();
+        }
+
+        public override void OnRemove()
+        {
+            EventManager.Instance.Broadcast(GameEvents.OnItemExplosion, _position, _itemID);
+
+            ObjectPool.Instance.ReturnItem(BoardItem, _itemID);
+            
+        }
+   
+    }
+}
