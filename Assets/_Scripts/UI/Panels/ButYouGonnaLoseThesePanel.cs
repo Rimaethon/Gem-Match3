ce0b2c8bd87a81bbf5d645ce178398917541ae73
Yoldaş Ulaş <yutalas@gmail.com>
2024-05-28 18:46:00 +0300

    UI Goal Prefab is Updated and ButYouGonnaLoseThesePanel.cs is created
    Instead of putting 4 Goal prefabs at start I will spawn them depending on the count of goals. Also now user can see and feel sorry about His/Her loses when decides not to buy additional moves.

---
+++ Assets/_Scripts/UI/Panels/ButYouGonnaLoseThesePanel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Scripts;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Scripts.UI.Panels
+{
+    public class ButYouGonnaLoseThesePanel:MonoBehaviour
+    {
+        [SerializeField] private GameObject progressGainedThisLevel;
+        [SerializeField] private GameObject eventProgressUIPrefab;
+        public Dictionary<int, int> itemsCollectedThisLevel;
+
+        private void OnEnable()
+        {
+            itemsCollectedThisLevel = SceneController.Instance.CollectedItems;
+            foreach (var item in itemsCollectedThisLevel)
+            {
+                GameObject progress = Instantiate(eventProgressUIPrefab, progressGainedThisLevel.transform);
+                progress.GetComponent<Image>().sprite=ObjectPool.Instance.GetItemSprite(item.Key);
+                progress.GetComponentInChildren<TextMeshProUGUI>().text = item.Value.ToString();
+            }
+        }
+        
+        
+    }
+}
