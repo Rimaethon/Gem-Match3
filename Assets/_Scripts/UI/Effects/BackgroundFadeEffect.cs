3796c9b7029f485f834a825c97dbb8c2a559c74f
Yoldaş Ulaş <yutalas@gmail.com>
2024-05-28 13:37:15 +0300

    BackgroundFadeEffect.cs and EventGoalRemovalEffect.cs are created
    EventGoalRemovalEffect is little bit resource intensive since I make that animation in UI instead of normal transform.

---
+++ Assets/_Scripts/UI/Effects/BackgroundFadeEffect.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using DG.Tweening;
+using Rimaethon.Scripts.Managers;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Rimaethon.Runtime.UI
+{
+    [RequireComponent(typeof(Image))]
+    public class BackgroundFadeEffect:MonoBehaviour
+    {
+        private Image _image;
+        
+        private void Awake()
+        {
+            _image = GetComponent<Image>();
+        }
+        private void OnEnable()
+        {
+            EventManager.Instance.AddHandler(GameEvents.FadeBackground, HandleFade);
+            EventManager.Instance.AddHandler(GameEvents.UnFadeBackground,HandleUnFade);
+        }
+        private void OnDisable()
+        {
+            if(EventManager.Instance == null)
+                return;
+            EventManager.Instance.RemoveHandler(GameEvents.FadeBackground, HandleFade);
+            EventManager.Instance.RemoveHandler(GameEvents.UnFadeBackground,HandleUnFade);
+        }
+        private void HandleUnFade()
+        {
+            _image.DOFade(0, 0.2f);
+        }
+
+        private void HandleFade()
+        {
+            _image.DOFade(0.9f, 0.2f);
+        }
+
+    }
+}
---
+++ Assets/_Scripts/UI/Effects/EventGoalRemovalEffect.cs
@@ -0,0 +1,34 @@
+﻿using DG.Tweening;
+using Sirenix.OdinInspector;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Rimaethon.Runtime.UI
+{
+    public class EventGoalRemovalEffect:MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _amountText;
+        private Image _image;
+        private readonly Vector3 _upwardMovementAmount = new Vector3(0, 100, 0);
+        private void Awake()
+        {
+            _image = GetComponent<Image>();
+        }
+        
+        [Button]
+        public void StartEffect(int itemID,int removalAmount,Vector3 position,bool isBooster)
+        {
+            _image.sprite = isBooster ? ObjectPool.Instance.GetBoosterSprite(itemID) : ObjectPool.Instance.GetItemSprite(itemID);
+            _amountText.text = "+"+removalAmount.ToString();
+            gameObject.GetComponent<RectTransform>().localPosition= position;
+            _image.enabled = true;
+            Debug.Log(gameObject.GetComponent<RectTransform>().position);
+            gameObject.GetComponent<RectTransform>().DOLocalMove(gameObject.GetComponent<RectTransform>().localPosition+_upwardMovementAmount, 1f).SetUpdate(UpdateType.Fixed).OnComplete(() =>
+            {
+                _image.enabled = false;
+                _amountText.text = "";
+            });
+        }
+    }
+}
