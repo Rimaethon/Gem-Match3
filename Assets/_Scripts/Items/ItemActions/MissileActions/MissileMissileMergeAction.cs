a39395d8f221af4e8e066047fa77a83ef59d41e1
Yoldaş Ulaş <yutalas@gmail.com>
2024-06-02 12:47:10 +0300

    MissileRocketAction is divided into MissileHorizontalRocketMergeAction.cs and MissileVerticalRocketMergeAction.cs, also MissileTntMergeAction.cs is created
    Yes somewhere in the past I forgot to add these into Version Control.

---
+++ Assets/_Scripts/Items/ItemActions/MissileActions/MissileMissileMergeAction.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using _Scripts.Managers;
+using Rimaethon.Scripts.Managers;
+using Scripts;
+using Scripts.BoosterActions;
+using UnityEngine;
+
+namespace _Scripts.Items.ItemActions.MergeActions
+{
+    public class MissileMissileMergeAction : IItemMergeAction
+    {
+        public bool IsFinished => _isFinished;
+        public int ItemID { get; set; }
+        public Board Board { get; set; }
+        public int Item1ID { get; set; }
+        public int Item2ID { get; set; }
+        private bool _isFinished;
+        public void InitializeMergeAction(Board board, int item1ID, int item2ID, Vector2Int position1, Vector2Int position2)
+        {
+            EventManager.Instance.Broadcast(GameEvents.AddActionToHandle,position2,item1ID,-1);
+            EventManager.Instance.Broadcast(GameEvents.AddActionToHandle,position2,item1ID,-1);
+            EventManager.Instance.Broadcast(GameEvents.AddActionToHandle,position2,item1ID,-1);
+            _isFinished = false;
+        }
+
+
+        public void Execute()
+        {
+            _isFinished = true;
+        }
+    }
+}
---
+++ Assets/_Scripts/Items/ItemActions/MissileActions/MissileVerticalRocketMergeAction.cs
@@ -0,0 +1,30 @@
+﻿using _Scripts.Managers;
+using Rimaethon.Scripts.Managers;
+using Scripts;
+using Scripts.BoosterActions;
+using UnityEngine;
+
+namespace _Scripts.Items.ItemActions.MergeActions
+{
+    public class MissileVerticalRocketMergeAction : IItemMergeAction
+    {
+        public bool IsFinished => _isFinished;
+        public int ItemID { get; set; }
+        public Board Board { get; set; }
+        public int Item1ID { get; set; }
+        public int Item2ID { get; set; }
+        private bool _isFinished;
+        public void InitializeMergeAction(Board board, int item1ID, int item2ID, Vector2Int position1, Vector2Int position2)
+        {
+            EventManager.Instance.Broadcast(GameEvents.AddActionToHandle,position2,item1ID,item2ID);
+
+            _isFinished = false;
+        }
+
+
+        public void Execute()
+        {
+            _isFinished = true;
+        }
+    }
+}
