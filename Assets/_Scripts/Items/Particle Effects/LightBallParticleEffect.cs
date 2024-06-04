b68d53bd67f2c41c3fe6a0c97a1a08b1985423b3
Yoldaş Ulaş <yutalas@gmail.com>
2024-06-04 08:34:55 +0300

    Item match Partıcle effects, light Ball Explosion Particle effect ,Light Ball Particle effect and their respective classes are updated
    Now LightBallParticleEffect is rotating and all of the Match Items are using texture sheet animations and Particle Material

---
+++ Assets/_Scripts/Items/Particle Effects/LightBallParticleEffect.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security;
+using DG.Tweening;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Scripts
+{
+    public class LightBallParticleEffect:BoosterParticleEffect
+    {
+        public ParticleSystem lightBallExplosionParticles;
+        public GameObject lightBallSprite;
+        public List<LightBallRay> lightBallRays;
+        public GameObject chargingParticleEffects;
+        private Tween _rotationTween;
+
+        public void OnEnable()
+        {
+            _rotationTween = lightBallSprite.transform.DOLocalRotate(new Vector3(0, 0, 1000), 1, RotateMode.FastBeyond360)
+                .SetLoops(-1, LoopType.Incremental)
+                .SetEase(Ease.Linear);
+            lightBallSprite.SetActive(true);
+            chargingParticleEffects.SetActive(true);
+        }
+
+        public  void OnDisable()
+        {
+            if(_rotationTween != null)
+            {
+                _rotationTween.Kill();
+                _rotationTween = null;
+            }
+        }
+    }
+}
---
+++ Assets/_Scripts/Items/Particle Effects/MatchItemParticleEffect.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Scripts
+{
+    public class MatchItemParticleEffect:ItemParticleEffect
+    {
+        private void OnParticleSystemStopped()
+        {
+            if(ObjectPool.Instance==null)
+                return;
+            ObjectPool.Instance.ReturnBoosterCreationEffect(gameObject);
+        }
+        private void Awake()
+        {
+            var particleSystem = GetComponent<ParticleSystem>().main;
+            particleSystem.stopAction = ParticleSystemStopAction.Callback;
+        }
+    }
+}
