5116a93b29396c32dd422d0c31d3fde4fd849644
Yoldaş Ulaş <yutalas@gmail.com>
2024-06-05 14:12:24 +0300

    QuitLevelPanel.cs and TryAgainPanel.cs are created for controlling the Panels
    TryAgainPanel class is showing the Level Panel with addition of showing the uncollected goals and Quit Level Panel is just showing a quit button and a broken Heart image

---
+++ Assets/_Scripts/UI/Panels/QuitLevelPanel.cs
@@ -0,0 +1,31 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+namespace Rimaethon.Runtime.UI
+{
+    public class QuitLevelPanel:MonoBehaviour
+    {
+        [SerializeField] private Button _quitButton;
+        [SerializeField] private Button _closePanelButton;
+        [SerializeField] private GameObject tryAgainPanel;
+        
+        private void OnEnable()
+        {
+            _quitButton.onClick.AddListener(HandleQuitButton);
+            _closePanelButton.onClick.AddListener(HandleClosePanelButton);
+        }
+
+        private void HandleClosePanelButton()
+        {
+            gameObject.SetActive(false);
+        }
+
+        private void HandleQuitButton()
+        {
+            gameObject.SetActive(false);
+            tryAgainPanel.SetActive(true);
+        }
+    }
+}
---
+++ Assets/_Scripts/UI/Panels/TryAgainPanel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Rimaethon.Runtime.UI
+{
+    public class TryAgainPanel:MonoBehaviour
+    {
+        [SerializeField] private GameObject tryAgainPanel;
+        [SerializeField] private TextMeshProUGUI levelText;
+        [SerializeField] private List<GameObject> goalsFailedToCollect;
+        
+        private void Start()
+        {
+            int[] goalIds = SaveManager.Instance.GetCurrentLevelGoalIds();
+            levelText.text = "Level " + SaveManager.Instance.GetCurrentLevelName();
+            for(int i=0;i<goalIds.Length;i++)
+            { 
+                goalsFailedToCollect[i].SetActive(true);
+                goalsFailedToCollect[i].GetComponent<Image>().sprite =
+                    ObjectPool.Instance.GetItemSprite(goalIds[i]);
+            }
+        }
+    }
+}
