88e710b12ae8bf1e4c1cc4a1359e5933e3949a30
Yoldaş Ulaş <yutalas@gmail.com>
2024-06-22 23:55:04 +0300

    Board Sprite Save Data Updated and CellType enum created
    Now It stores a CellType matrix for determining which cell is blank,shifter,spawner or normal. Also it has a inspector tool for setting the matrix.

--- Assets/_Scripts/Data Classes/BoardSpriteSaveData.cs
+++ Assets/_Scripts/Data Classes/BoardSpriteSaveData.cs
@@ -1,21 +1,57 @@
 ﻿using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEditor;
 using UnityEngine;
 
 namespace _Scripts.Data_Classes
 {
     public class BoardSpriteSaveData
     {
-        public int Width;
-        public int Height;
-        public HashSet<Vector2Int> BlankCells;
+        public int Width = 8;
+        public int Height = 10;
+        [TableMatrix(DrawElementMethod = "DrawCell", ResizableColumns = true)]
+        public CellType[,] CellValues;
         public Sprite Sprite;
-        public BoardSpriteSaveData(Sprite sprite, int width, int height, HashSet<Vector2Int> blankCells)
+        [Button]
+        public void ResetCells()
         {
-            Sprite = sprite;
-            Width = width;
-            Height = height;
-            BlankCells = blankCells;
+            CellValues = new CellType[Width, Height];
         }
-        
+        private CellType DrawCell(Rect rect, CellType value)
+        {
+            if (Event.current.type == EventType.MouseDown && rect.Contains(Event.current.mousePosition))
+            {
+                if ( CellType.NORMAL == value)
+                {
+                    value = CellType.SPAWNER;
+                }
+                else if ( CellType.SPAWNER == value)
+                {
+                    value = CellType.BLANK;
+                }
+                else if (CellType.BLANK == value)
+                {
+                    value = CellType.SHIFTER;
+                }
+                else if(CellType.SHIFTER==value)
+                    value = (int)CellType.NORMAL;
+                GUI.changed = true;
+                Event.current.Use();
+            }
+            EditorGUI.DrawRect(rect, value == CellType.SPAWNER   ? Color.green
+                                     : value ==CellType.BLANK   ? Color.red
+                                     : value == CellType.SHIFTER ? Color.yellow
+                                                                        : Color.gray);
+
+            return value;
+        }
+
+    }
+    public enum CellType
+    {
+        NORMAL=0,
+        SPAWNER=1,
+        BLANK=2,
+        SHIFTER=3
     }
 }
