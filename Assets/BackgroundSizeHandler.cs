54358c1307095a889011b58acc76f6fdd28120c5
Yoldaş Ulaş <yutalas@gmail.com>
2024-06-28 15:50:48 +0300

    BackgroundSizeHandler and CameraSizeAdjuster are created
    In some aspect ratios Board was looking too big then it should be so I wrote 2 scripts for adjusting the background and Camera size depending on the aspect ratio of the screen.

---
+++ Assets/BackgroundSizeHandler.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundSizeHandler : MonoBehaviour
+{
+	private const float default_aspect = 1.777f;
+
+	private void Awake()
+	{
+		float aspect = (float)Screen.height/(float)Screen.width  ;
+		transform.localScale=Vector3.one*(aspect)/default_aspect;
+	}
+}
